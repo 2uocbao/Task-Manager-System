d63c0dcefe0663de7dc9b63b5e0feca4eaf9fd22
2uocbao <2uocbao.it@gmail.com>
2025-09-06 11:47:41 +0700

    Add team member specification

---
+++ src/main/java/com/quocbao/taskmanagementsystem/specifications/TeamMemberSpecification.java
@@ -0,0 +1,28 @@
+package com.quocbao.taskmanagementsystem.specifications;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.quocbao.taskmanagementsystem.entity.TeamMember;
+import com.quocbao.taskmanagementsystem.entity.TeamMember_;
+import com.quocbao.taskmanagementsystem.entity.Team_;
+import com.quocbao.taskmanagementsystem.entity.User_;
+
+public class TeamMemberSpecification {
+
+	private TeamMemberSpecification() {
+
+	}
+
+	public static Specification<TeamMember> getTeamMemberById(Long id) {
+		return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.get(TeamMember_.id), id);
+	}
+	
+	public static Specification<TeamMember> getTeamMemberByUserId(Long userId) {
+		return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.get(TeamMember_.user).get(User_.id), userId);
+	}
+
+	public static Specification<TeamMember> getTeamMemberByTeamId(Long teamId) {
+		return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.get(TeamMember_.team).get(Team_.id), teamId);
+	}
+
+}
