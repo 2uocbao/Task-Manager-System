5a4bca883627d0603385ef2f4d88298ca7e4b769
2uocbao <2uocbao.it@gmail.com>
2025-09-06 12:38:10 +0700

    Update duplicate exception

--- src/main/java/com/quocbao/taskmanagementsystem/exception/DuplicateException.java
+++ src/main/java/com/quocbao/taskmanagementsystem/exception/DuplicateException.java
@@ -1,14 +1,17 @@
 package com.quocbao.taskmanagementsystem.exception;
 
+import java.io.Serial;
+
 public class DuplicateException extends RuntimeException {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     * 
+     */
+    @Serial
+    private static final long serialVersionUID = 1L;
 
-	public DuplicateException(String message) {
-		super(message);
-	}
+    public DuplicateException(String message) {
+        super(message);
+    }
 
 }
