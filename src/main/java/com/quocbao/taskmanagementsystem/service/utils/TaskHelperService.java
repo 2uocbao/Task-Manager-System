b368884b14f90341cc7911a44deb1fdbf51d230b
2uocbao <2uocbao.it@gmail.com>
2025-09-06 12:12:09 +0700

    Update task helper service

--- src/main/java/com/quocbao/taskmanagementsystem/service/utils/TaskHelperService.java
+++ src/main/java/com/quocbao/taskmanagementsystem/service/utils/TaskHelperService.java
@@ -4,8 +4,8 @@ import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-
 import com.quocbao.taskmanagementsystem.common.IdEncoder;
 import com.quocbao.taskmanagementsystem.entity.Task;
 import com.quocbao.taskmanagementsystem.repository.TaskRepository;
@@ -13,22 +13,38 @@ import com.quocbao.taskmanagementsystem.specifications.TaskSpecification;
 
 @Service
 public class TaskHelperService {
-	
+
 	private final TaskRepository taskRepository;
 	private final IdEncoder idEncoder;
-	
+
 	public TaskHelperService(TaskRepository taskRepository, IdEncoder idEncoder) {
 		this.taskRepository = taskRepository;
 		this.idEncoder = idEncoder;
 	}
 
-	
-	public Optional<Task> existTask(String taskId) {
+	public Optional<Task> getTask(String taskId) {
 		return taskRepository.findById(idEncoder.decode(taskId));
 	}
 
-	
+	public Boolean isTaskExist(String taskId) {
+		return taskRepository.exists(Specification.where(TaskSpecification.getTaskById(idEncoder.decode(taskId))));
+	}
+
+	public Boolean isCreatorTask(Long userId, String taskId) {
+		return taskRepository.exists(Specification.where(TaskSpecification.getTaskById(idEncoder.decode(taskId))
+				.and(TaskSpecification.getTaskByUserId(userId))));
+	}
+
 	public List<Task> getTaskBetweenDate(Timestamp startAt, Timestamp endAt) {
 		return taskRepository.findAll(TaskSpecification.getTaskByDate(startAt, endAt));
+	}
+	
+	public List<Task> getTaskByTeamId(String teamId)  {
+		return taskRepository.findAll(TaskSpecification.getTaskByTeamId(idEncoder.decode(teamId)));
+	}
+
+	public Boolean isTaskActive(String teamId, String status) {
+		return taskRepository.exists(Specification.where(TaskSpecification.getTaskByTeamId(idEncoder.decode(teamId))
+				.and(TaskSpecification.getTaskNotHaveStatus(status))));
 	}
 }
