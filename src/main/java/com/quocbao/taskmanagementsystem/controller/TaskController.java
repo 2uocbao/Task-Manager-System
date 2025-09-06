d103f512ad278d023c170ce8d6f351fda163588f
2uocbao <2uocbao.it@gmail.com>
2025-09-06 11:09:05 +0700

    Update task controller

--- src/main/java/com/quocbao/taskmanagementsystem/controller/TaskController.java
+++ src/main/java/com/quocbao/taskmanagementsystem/controller/TaskController.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.quocbao.taskmanagementsystem.common.DataResponse;
 import com.quocbao.taskmanagementsystem.common.PaginationResponse;
-import com.quocbao.taskmanagementsystem.payload.request.AssignRequest;
 import com.quocbao.taskmanagementsystem.payload.request.TaskRequest;
 import com.quocbao.taskmanagementsystem.payload.response.TaskResponse;
 import com.quocbao.taskmanagementsystem.service.TaskService;
@@ -36,16 +34,15 @@ public class TaskController {
 		this.taskService = taskSerivce;
 	}
 
-	@PostMapping("/tasks")
-	public DataResponse createTask(@RequestBody TaskRequest taskRequest) {
-		TaskResponse taskResponse = taskService.createTask(taskRequest);
-		EntityModel<TaskResponse> entityModel = EntityModel.of(taskResponse);
-		return new DataResponse(HttpStatus.OK.value(), entityModel, "Task creation request successful.");
+	@PostMapping("/teams/{teamId}/tasks")
+	public DataResponse createTask(@PathVariable String teamId, @RequestBody TaskRequest taskRequest) {
+		TaskResponse taskResponse = taskService.createTask(teamId, taskRequest);
+		return new DataResponse(HttpStatus.OK.value(), taskResponse, "Task creation request successful.");
 	}
 
-	@GetMapping("/users/{userId}/tasks/{taskId}")
-	public DataResponse getTask(@PathVariable String userId, @PathVariable String taskId) {
-		TaskResponse taskResponse = taskService.getTask(userId, taskId);
+	@GetMapping("/tasks/{taskId}")
+	public DataResponse getTask(@PathVariable String taskId) {
+		TaskResponse taskResponse = taskService.getTask(taskId);
 		return new DataResponse(HttpStatus.OK.value(), taskResponse, "Retrieve info Task.");
 	}
 
@@ -55,25 +52,25 @@ public class TaskController {
 		return new DataResponse(HttpStatus.OK.value(), taskResponse, "Update task request successful.");
 	}
 
-	@DeleteMapping("/users/{userId}/tasks/{taskId}")
-	public DataResponse deleteTask(@PathVariable String userId, @PathVariable String taskId) {
-		return new DataResponse(HttpStatus.OK.value(), taskService.deleteTask(userId, taskId), "Request successful.");
+	@DeleteMapping("/tasks/{taskId}")
+	public DataResponse deleteTask(@PathVariable String taskId) {
+		taskService.deleteTask(taskId);
+		return new DataResponse(HttpStatus.OK.value(), null, "Request successful.");
 	}
 
-	@GetMapping("/users/{userId}/tasks")
-	public PaginationResponse<TaskResponse> getTasks(@PathVariable String userId,
+	@GetMapping("/teams/{teamId}/tasks")
+	public PaginationResponse<TaskResponse> getTasks(@PathVariable String teamId,
 			@RequestParam(required = true) String startDate, @RequestParam(required = true) String endDate,
 			@RequestParam(required = false, defaultValue = "PENDING") String status,
 			@RequestParam(required = false, defaultValue = "LOW") String priority,
-			@RequestParam(required = false, defaultValue = "fasle") Boolean assign,
 			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
 
 		Direction direction = Direction.fromString(Direction.DESC.toString());
 
-		Page<TaskResponse> taskPage = taskService.getTasks(userId, status, priority, startDate, endDate, assign,
+		Page<TaskResponse> taskPage = taskService.getTasks(teamId, status, priority, startDate, endDate,
 				PageRequest.of(page, size, Sort.by(direction, "dueAt")));
 
-		List<TaskResponse> entityModels = taskPage.getContent().stream().toList();
+		List<TaskResponse> entityModels = taskPage.getContent();
 
 		PaginationResponse<TaskResponse> paginationResponse = new PaginationResponse<>(HttpStatus.OK, entityModels,
 				taskPage.getPageable().getPageNumber(), taskPage.getSize(), taskPage.getTotalElements(),
@@ -83,41 +80,24 @@ public class TaskController {
 		return paginationResponse;
 	}
 
-	@PutMapping("/tasks/{taskId}/assignees")
-	public DataResponse asignTaskToUser(@PathVariable String taskId, @RequestBody AssignRequest assignRequest) {
-		taskService.addUser(taskId, assignRequest.getUserId(), assignRequest.getToUserId());
-		return new DataResponse(HttpStatus.OK.value(), null, "Successful");
+	@PutMapping("/tasks/{taskId}/status")
+	public DataResponse updateStatus(@PathVariable String taskId, @RequestBody TaskRequest taskRequest) {
+		return new DataResponse(HttpStatus.OK.value(), taskService.updateStatus(taskId, taskRequest), "Success");
 	}
 
-	@PostMapping("/tasks/{taskId}/assignees")
-	public DataResponse removeUserInTask(@PathVariable String taskId, @RequestBody AssignRequest assignRequest) {
-		taskService.removeUser(taskId, assignRequest.getUserId(), assignRequest.getToUserId());
-		return new DataResponse(HttpStatus.OK.value(), null, "Successful");
+	@PutMapping("/tasks/{taskId}/priority")
+	public DataResponse updatePriority(@PathVariable String taskId, @RequestBody TaskRequest taskRequest) {
+		return new DataResponse(HttpStatus.OK.value(), taskService.updatePriority(taskId, taskRequest), "Success");
 	}
 
-	@PutMapping("/users/{userId}/tasks/{taskId}/status")
-	public DataResponse updateStatus(@PathVariable String userId, @PathVariable String taskId,
-			@RequestBody TaskRequest taskRequest) {
-		return new DataResponse(HttpStatus.OK.value(),
-				taskService.updateStatus(taskId, userId, taskRequest), "Success");
-	}
-
-	@PutMapping("/users/{userId}/tasks/{taskId}/priority")
-	public DataResponse updatePriority(@PathVariable String userId, @PathVariable String taskId,
-			@RequestBody TaskRequest taskRequest) {
-		return new DataResponse(HttpStatus.OK.value(),
-				taskService.updatePriority(taskId, userId, taskRequest), "Success");
-	}
-
-	@GetMapping("/users/{userId}/tasks/searchWith")
-	public PaginationResponse<TaskResponse> searchTask(@PathVariable String userId, @RequestParam boolean type,
-			@RequestParam(required = false) String keySearch, @RequestParam(defaultValue = "0") int page,
+	@GetMapping("/tasks/searchs")
+	public PaginationResponse<TaskResponse> searchTask(
+			@RequestParam(required = false) String keyword, @RequestParam(defaultValue = "0") int page,
 			@RequestParam(defaultValue = "10") int size) {
-
 		Pageable pageable = PageRequest.of(page, size);
-		Page<TaskResponse> taskResponses = taskService.searchTasks(userId, keySearch, type, pageable);
+		Page<TaskResponse> taskResponses = taskService.searchTasks(keyword, pageable);
 
-		List<TaskResponse> taskResponseList = taskResponses.toList();
+		List<TaskResponse> taskResponseList = taskResponses.getContent();
 
 		return new PaginationResponse<TaskResponse>(HttpStatus.OK, taskResponseList,
 				taskResponses.getPageable().getPageNumber(), taskResponses.getSize(), taskResponses.getTotalElements(),
