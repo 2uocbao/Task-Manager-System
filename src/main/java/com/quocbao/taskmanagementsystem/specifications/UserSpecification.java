b34092db5b9adbdbaa46952231e539027f955ddd
2uocbao <2uocbao.it@gmail.com>
2025-09-06 11:48:45 +0700

    Update user specification

--- src/main/java/com/quocbao/taskmanagementsystem/specifications/UserSpecification.java
+++ src/main/java/com/quocbao/taskmanagementsystem/specifications/UserSpecification.java
@@ -1,7 +1,16 @@
 package com.quocbao.taskmanagementsystem.specifications;
 
+import org.springframework.data.jpa.domain.Specification;
+
+import com.quocbao.taskmanagementsystem.entity.User;
+import com.quocbao.taskmanagementsystem.entity.User_;
+
 public class UserSpecification {
 
 	private UserSpecification() {
+	}
+
+	public static Specification<User> getUserById(Long userId) {
+		return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.get(User_.id), userId);
 	}
 }
