1242dd6fbf0facc43fbfea33d8aab362ef57633f
2uocbao <2uocbao.it@gmail.com>
2025-09-06 11:08:39 +0700

    Add task assignment controller

---
+++ src/main/java/com/quocbao/taskmanagementsystem/controller/TaskAssignmentController.java
@@ -0,0 +1,47 @@
+package com.quocbao.taskmanagementsystem.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quocbao.taskmanagementsystem.common.DataResponse;
+import com.quocbao.taskmanagementsystem.common.PaginationResponse;
+import com.quocbao.taskmanagementsystem.payload.request.TaskAssignRequest;
+import com.quocbao.taskmanagementsystem.payload.response.TaskAssignResponse;
+import com.quocbao.taskmanagementsystem.service.TaskAssignService;
+
+@RestController
+public class TaskAssignmentController {
+
+	private final TaskAssignService taskAssignService;
+
+	public TaskAssignmentController(TaskAssignService taskAssignService) {
+		this.taskAssignService = taskAssignService;
+	}
+
+	@PostMapping("/tasks/{taskId}/task_assignments")
+	public DataResponse createTaskAssign(@PathVariable String taskId,
+			@RequestBody TaskAssignRequest taskAssignRequest) {
+		return new DataResponse(HttpStatus.OK.value(), taskAssignService.addAssign(taskId, taskAssignRequest),
+				"Success");
+	}
+
+	@PutMapping("/tasks/{taskId}/task_assignments/{taskAssignId}")
+	public DataResponse deleteTaskAssign(@PathVariable String taskId, @PathVariable String taskAssignId,
+			@RequestBody TaskAssignRequest taskAssignRequest) {
+		return new DataResponse(HttpStatus.OK.value(),
+				taskAssignService.removeAssign(taskId, taskAssignId, taskAssignRequest), "Success");
+	}
+
+	@GetMapping("/tasks/{taskId}/task_assignments")
+	public PaginationResponse<TaskAssignResponse> getTaskAssigns(@PathVariable String taskId) {
+		List<TaskAssignResponse> taskAssignResponses = taskAssignService.getTaskAssigns(taskId);
+		return new PaginationResponse<>(HttpStatus.OK, taskAssignResponses);
+	}
+}
