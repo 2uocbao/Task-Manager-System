efcfbcb3dcda02d39014e57d63f8f44fcb9faaff
2uocbao <2uocbao.it@gmail.com>
2025-09-06 12:27:16 +0700

    Update report request

--- src/main/java/com/quocbao/taskmanagementsystem/payload/request/ReportRequest.java
+++ src/main/java/com/quocbao/taskmanagementsystem/payload/request/ReportRequest.java
@@ -7,13 +7,10 @@ import lombok.Getter;
 @Getter
 public class ReportRequest {
 
-	@JsonProperty("user_id")
-	private String userId;
-	
 	@JsonProperty("task_id")
 	private String taskId;
-	
+
 	@JsonProperty("external_url")
 	private String externalUrl;
-	
+
 }
