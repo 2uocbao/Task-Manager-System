f3c1a3fac932b280196eab2afdd874036d62aee9
2uocbao <2uocbao.it@gmail.com>
2025-09-06 12:02:37 +0700

    Update notifi service

--- src/main/java/com/quocbao/taskmanagementsystem/service/NotificationService.java
+++ src/main/java/com/quocbao/taskmanagementsystem/service/NotificationService.java
@@ -3,15 +3,18 @@ package com.quocbao.taskmanagementsystem.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.quocbao.taskmanagementsystem.entity.Notification;
 import com.quocbao.taskmanagementsystem.payload.response.NotifiResponse;
 
 public interface NotificationService {
-
-	public void updateStatusNotification(String userId, Long notificationId);
 	
-	public void updateStatusAll(String userId);
+	public void saveNotification(Notification notification);
 
-	Page<NotifiResponse> getNotifications(String userId, String type, Boolean status, Pageable pageable);
+	public void updateStatusNotification(Long notificationId);
+	
+	public void updateStatusAll();
 
-	public boolean haveUnRead(String userId);
+	Page<NotifiResponse> getNotifications(String type, Boolean status, Pageable pageable);
+
+	public boolean haveUnRead();
 }
