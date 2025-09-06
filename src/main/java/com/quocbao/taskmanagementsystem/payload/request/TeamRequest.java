d1993bec35275748a510fcf61bab79d9b6bbb6ae
2uocbao <2uocbao.it@gmail.com>
2025-09-06 12:29:17 +0700

    Add team request

---
+++ src/main/java/com/quocbao/taskmanagementsystem/payload/request/TeamRequest.java
@@ -0,0 +1,14 @@
+package com.quocbao.taskmanagementsystem.payload.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TeamRequest {
+
+	@JsonProperty("name")
+	private String name;
+}
