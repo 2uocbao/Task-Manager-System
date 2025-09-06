ab7063d10264da6582514787660404e9cfc38ea5
2uocbao <2uocbao.it@gmail.com>
2025-09-06 11:12:26 +0700

    Update comment

--- src/main/java/com/quocbao/taskmanagementsystem/entity/Comment.java
+++ src/main/java/com/quocbao/taskmanagementsystem/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.quocbao.taskmanagementsystem.entity;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
@@ -17,6 +18,8 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,45 +27,42 @@ import lombok.Setter;
 @Table(name = "comment")
 @Getter
 @Setter
-public class Comment implements Serializable{
+@Builder
+@AllArgsConstructor
+public class Comment implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     * 
+     */
+    @Serial
+    private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "task_id", nullable = false)
 	private Task task;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
-	
+
 	@Column(name = "text")
 	private String text;
-	
+
 	@CreationTimestamp
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "created_at")
 	private Timestamp createdAt;
-	
+
 	@UpdateTimestamp
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "updated_at")
 	private Timestamp updatedAt;
-	
+
 	public Comment() {
-		
-	}
-	
-	public Comment(User user, Task task, String text) {
-		this.user = user;
-		this.task = task;
-		this.text = text;
+
 	}
 }
