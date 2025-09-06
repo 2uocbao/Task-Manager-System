780d3d7956d5afae7a4f651ee8198034b0b3a1fa
2uocbao <2uocbao.it@gmail.com>
2025-09-06 12:45:00 +0700

    Add task event

---
+++ src/main/java/com/quocbao/taskmanagementsystem/events/NotifiEvent/TaskEvent.java
@@ -0,0 +1,28 @@
+package com.quocbao.taskmanagementsystem.events.NotifiEvent;
+
+import lombok.Getter;
+
+@Getter
+public class TaskEvent {
+
+	private final Long taskId;
+	private final Long senderId;
+	private final Long receiverId;
+	private final String senderName;
+	private final String taskTitle;
+	private final String contentType;
+	private final String token;
+	private final String language;
+
+	public TaskEvent(Long taskId, Long senderId, Long receiverId, String senderName, String taskTitle, String contentType,
+			String token, String language) {
+		this.taskId = taskId;
+		this.senderId = senderId;
+		this.receiverId = receiverId;
+		this.senderName = senderName;
+		this.taskTitle = taskTitle;
+		this.contentType = contentType;
+		this.token = token;
+		this.language = language;
+	}
+}
