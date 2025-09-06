1452e1f71a0f9d185e57d97280cdb08b201bc21d
2uocbao <2uocbao.it@gmail.com>
2025-09-06 11:57:37 +0700

    Update user service impl

--- src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.quocbao.taskmanagementsystem.serviceimpl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -11,50 +14,64 @@ import com.quocbao.taskmanagementsystem.payload.request.FcmRequest;
 import com.quocbao.taskmanagementsystem.payload.response.UserResponse;
 import com.quocbao.taskmanagementsystem.repository.UserRepository;
 import com.quocbao.taskmanagementsystem.service.UserService;
-
-import jakarta.transaction.Transactional;
+import com.quocbao.taskmanagementsystem.service.utils.AuthenticationService;
 
 @Service
-@Transactional
 public class UserServiceImpl implements UserService {
 
+	static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+
 	private final UserRepository userRepository;
+
+	private final AuthenticationService authService;
+
 	private final IdEncoder idEncoder;
 
-	public UserServiceImpl(UserRepository userRepository, IdEncoder idEncoder) {
+	public UserServiceImpl(UserRepository userRepository, AuthenticationService authService, IdEncoder idEncoder) {
 		this.userRepository = userRepository;
+		this.authService = authService;
 		this.idEncoder = idEncoder;
-	}
-
-	@Override
-	public Page<UserResponse> searchUser(String userId, String keySearch, Pageable pageable) {
-		return userRepository.searchUser(idEncoder.decode(userId), keySearch, pageable)
-				.map(t -> new UserResponse(idEncoder.endcode(t.getId()), t.getFirst(), t.getLast(), t.getImage(),
-						t.getEmail()));
 	}
 
 	@Override
 	public User getUserByEmail(String email) {
 		User user = userRepository.findByEmail(email);
 		if (user == null) {
-			throw new ResourceNotFoundException("User not found");
+			throw new ResourceNotFoundException("User can not found");
 		}
 		return user;
 	}
 
 	@Override
-	public void addToken(FcmRequest fcmRequest) {
-		userRepository.findById(idEncoder.decode(fcmRequest.getUserId())).ifPresentOrElse(t -> {
-			t.setToken(fcmRequest.getFcmToken());
-			t.setLanguage(fcmRequest.getLanguage());
-			userRepository.save(t);
-		}, () -> new ResourceNotFoundException());
+	public Page<UserResponse> searchUser(String keyword, Pageable pageable) {
+		return userRepository.searchUser(authService.getUserIdInContext(), keyword, pageable)
+				.map(t -> new UserResponse(idEncoder.encode(t.getId()), t.getFirst(), t.getLast(), t.getImage(),
+						t.getEmail()));
 	}
 
 	@Override
-	public UserResponse getUser(String userId) {
-		User user = userRepository.findById(idEncoder.decode(userId)).get();
-		return new UserResponse(userId, user.getFirstName(), user.getLastName(), user.getImage(), null);
+	public void addToken(FcmRequest fcmRequest) {
+		userRepository.findById(authService.getUserIdInContext()).ifPresent(t -> {
+			t.setToken(fcmRequest.getFcmToken());
+			t.setLanguage(fcmRequest.getLanguage());
+			userRepository.save(t);
+		});
 	}
 
+	@Override
+	@Cacheable(value = "userId", key = "#userId")
+	public User getUser(String userId) {
+		return userRepository.findById(idEncoder.decode(userId)).get();
+	}
+
+	@Override
+	public void removeToken() {
+		Long currentUserId = authService.getUserIdInContext();
+		userRepository.findById(currentUserId).ifPresent(t -> {
+			t.setToken(null);
+			t.setLanguage(null);
+			userRepository.save(t);
+		});
+		LOGGER.info("Remove token of " + currentUserId);
+	}
 }
