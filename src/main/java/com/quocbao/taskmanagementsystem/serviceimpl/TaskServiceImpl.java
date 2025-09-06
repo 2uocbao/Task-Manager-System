1d813bc8fd1be93f07ee52097c5eaee8c6266e91
2uocbao <2uocbao.it@gmail.com>
2025-09-06 11:55:13 +0700

    Update task service impl

--- src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/TaskServiceImpl.java
+++ src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/TaskServiceImpl.java
@@ -1,8 +1,6 @@
 package com.quocbao.taskmanagementsystem.serviceimpl;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
@@ -18,237 +16,200 @@ import com.quocbao.taskmanagementsystem.common.NotificationType;
 import com.quocbao.taskmanagementsystem.common.PriorityEnum;
 import com.quocbao.taskmanagementsystem.common.StatusEnum;
 import com.quocbao.taskmanagementsystem.entity.Task;
+import com.quocbao.taskmanagementsystem.entity.Team;
 import com.quocbao.taskmanagementsystem.entity.User;
-import com.quocbao.taskmanagementsystem.events.TaskEvent;
+import com.quocbao.taskmanagementsystem.events.DeleteEvent.NotifiDeletedEvent;
+import com.quocbao.taskmanagementsystem.events.DeleteEvent.ReportDeletedEvent;
+import com.quocbao.taskmanagementsystem.exception.DuplicateException;
+import com.quocbao.taskmanagementsystem.exception.ForbiddenException;
 import com.quocbao.taskmanagementsystem.exception.ResourceNotFoundException;
 import com.quocbao.taskmanagementsystem.payload.request.TaskRequest;
 import com.quocbao.taskmanagementsystem.payload.response.TaskResponse;
+import com.quocbao.taskmanagementsystem.payload.response.TaskStatusResponse;
 import com.quocbao.taskmanagementsystem.repository.TaskRepository;
 import com.quocbao.taskmanagementsystem.service.TaskService;
-import com.quocbao.taskmanagementsystem.service.utils.CommentHelperService;
-import com.quocbao.taskmanagementsystem.service.utils.ReportHelperService;
-import com.quocbao.taskmanagementsystem.service.utils.UserHelperService;
+import com.quocbao.taskmanagementsystem.service.utils.AuthenticationService;
+import com.quocbao.taskmanagementsystem.service.utils.TaskAssignmentHelperService;
+import com.quocbao.taskmanagementsystem.service.utils.TeamHelperService;
+import com.quocbao.taskmanagementsystem.service.utils.TeamMemberHelperService;
 import com.quocbao.taskmanagementsystem.specifications.TaskSpecification;
 
 @Service
 @Transactional
 public class TaskServiceImpl implements TaskService {
 
+	private final ApplicationEventPublisher applicationEventPublisher;
+
 	private final TaskRepository taskRepository;
 
-	private final CommentHelperService commentHelperService;
+	private final TeamHelperService teamHelperService;
 
-	private final ReportHelperService reportHelperService;
+	private final TeamMemberHelperService teamMemberHelperService;
+
+	private final TaskAssignmentHelperService taskAssignmentHelperService;
+
+	private final AuthenticationService authService;
 
 	private final MethodGeneral methodGeneral;
 
-	private final UserHelperService userHelperService;
-
 	private final IdEncoder idEncoder;
 
-	private final ApplicationEventPublisher applicationEventPublisher;
-
-	public TaskServiceImpl(TaskRepository taskRepository, UserHelperService userHelperService,
-			MethodGeneral methodGeneral, CommentHelperService commentHelperService,
-			ReportHelperService reportHelperService, IdEncoder idEncoder,
-			ApplicationEventPublisher applicationEventPublisher) {
-		this.taskRepository = taskRepository;
-		this.methodGeneral = methodGeneral;
-		this.commentHelperService = commentHelperService;
-		this.reportHelperService = reportHelperService;
-		this.userHelperService = userHelperService;
-		this.idEncoder = idEncoder;
+	public TaskServiceImpl(ApplicationEventPublisher applicationEventPublisher, TaskRepository taskRepository,
+			TeamHelperService teamHelperService, TaskAssignmentHelperService taskAssignmentHelperService,
+			TeamMemberHelperService teamMemberHelperService, AuthenticationService authService,
+			MethodGeneral methodGeneral, IdEncoder idEncoder) {
 		this.applicationEventPublisher = applicationEventPublisher;
+		this.taskRepository = taskRepository;
+		this.teamHelperService = teamHelperService;
+		this.teamMemberHelperService = teamMemberHelperService;
+		this.taskAssignmentHelperService = taskAssignmentHelperService;
+		this.authService = authService;
+		this.methodGeneral = methodGeneral;
+		this.idEncoder = idEncoder;
 	}
 
 	@Override
-	public TaskResponse createTask(TaskRequest taskRequest) {
-		User user = userHelperService.userExist(taskRequest.getCreatorId())
-				.orElseThrow(() -> new ResourceNotFoundException("User can not found"));
-		Task task = new Task(taskRequest);
-		task.setUser(user);
-		return new TaskResponse(taskRepository.save(task));
-
-	}
-
-	@Override
-	public TaskResponse getTask(String userId, String taskId) {
-		Task task = taskRepository.findById(idEncoder.decode(taskId))
-				.orElseThrow(() -> new ResourceNotFoundException("Task not found"));
-		if (task.getAssignTo() != null) {
-			methodGeneral.havePermission(idEncoder.decode(userId), task.getUser().getId(), task.getAssignTo().getId());
-		} else {
-			methodGeneral.validatePermission(idEncoder.decode(userId), task.getUser().getId());
+	public TaskResponse createTask(String teamId, TaskRequest taskRequest) {
+		try {
+			Long currentUserId = authService.getUserIdInContext();
+			if (!teamHelperService.isLeaderOfTeam(currentUserId, teamId)) {
+				throw new ForbiddenException("User can not create new task in this team");
+			}
+			Task task = new Task(taskRequest);
+			User user = User.builder().id(currentUserId).build();
+			Team team = Team.builder().id(idEncoder.decode(teamId)).build();
+			task.setTeam(team);
+			task.setUser(user);
+			task = taskRepository.save(task);
+			taskAssignmentHelperService.addTeamMember(currentUserId, task.getId());
+			return new TaskResponse(task);
+		} catch (Exception e) {
+			throw new ResourceNotFoundException();
 		}
-		TaskResponse taskResponse = new TaskResponse(task);
-		Optional.ofNullable(task.getAssignTo()).ifPresent(assignTo -> {
-			String assignName = Optional.ofNullable(assignTo.getFirstName()).orElse("") + " "
-					+ Optional.ofNullable(assignTo.getLastName()).orElse("");
-			String image = Optional.ofNullable(assignTo.getImage()).orElse("");
-			taskResponse.setUsername(assignName.trim());
-			taskResponse.setAssignTo(idEncoder.endcode(assignTo.getId()));
-			taskResponse.setImage(image);
-		});
-		taskResponse.setId(idEncoder.endcode(task.getId()));
-		taskResponse.setUserId(idEncoder.endcode(task.getUser().getId()));
-		return taskResponse;
+	}
+
+	@Override
+	public TaskResponse getTask(String taskId) {
+		Long currentUserId = authService.getUserIdInContext();
+		if (!taskAssignmentHelperService.isUserInTask(currentUserId, taskId)) {
+			isCreatorTask(currentUserId, taskId);
+		}
+		return taskRepository.findById(idEncoder.decode(taskId)).map(task -> {
+			TaskResponse taskResponse = new TaskResponse(task);
+			taskResponse.setId(idEncoder.encode(task.getId()));
+			taskResponse.setUserId(idEncoder.encode(task.getUser().getId()));
+			return taskResponse;
+		}).orElseThrow(() -> new ResourceNotFoundException("Task not found"));
 	}
 
 	@Override
 	public TaskResponse updateTask(String taskId, TaskRequest taskRequest) {
+		Long currentUserId = authService.getUserIdInContext();
+		isCreatorTask(currentUserId, taskId);
 		return taskRepository.findById(idEncoder.decode(taskId)).map(task -> {
-			methodGeneral.validatePermission(idEncoder.decode(taskRequest.getCreatorId()), task.getUser().getId());
 			task = task.updateTask(taskRequest);
 			return new TaskResponse(taskRepository.save(task));
 		}).orElseThrow(() -> new ResourceNotFoundException("Task not found for update."));
 	}
 
 	@Override
-	public String deleteTask(String userId, String taskId) {
-		return taskRepository.findById(idEncoder.decode(taskId)).map(t -> {
-			methodGeneral.validatePermission(idEncoder.decode(userId), t.getUser().getId());
-			taskRepository.delete(Task.builder().id(idEncoder.decode(taskId)).build());
-			return "Request Successful";
-		}).orElseThrow(() -> new ResourceNotFoundException("Not found task need delete."));
+	@Transactional
+	public void deleteTask(String taskId) {
+		Long currentUserId = authService.getUserIdInContext();
+		Long taskIdLong = idEncoder.decode(taskId);
+		taskRepository.findById(taskIdLong).ifPresentOrElse(t -> {
+			isCreatorTask(currentUserId, taskId);
+			if (taskAssignmentHelperService.haveAssign(taskId))
+				throw new DuplicateException("Remove the member from this task before deleting it.");
+			applicationEventPublisher
+					.publishEvent(new NotifiDeletedEvent(taskIdLong, NotificationType.TASK.toString()));
+			applicationEventPublisher.publishEvent(new ReportDeletedEvent(taskIdLong));
+			taskRepository.delete(t);
+
+		}, () -> new ResourceNotFoundException("Not found task need delete."));
 	}
 
 	@Override
-	public Page<TaskResponse> getTasks(String userId, String status, String priority, String startDate, String endDate,
-			Boolean assign, Pageable pageable) {
-
-		if (Boolean.TRUE.equals(assign)) {
-			Page<Task> tasks = listTasksAssign(userId, status, priority, startDate, endDate, pageable);
-			return result(tasks);
+	public Page<TaskResponse> getTasks(String teamId, String status, String priority, String startDate, String endDate,
+			Pageable pageable) {
+		Long currentUserId = authService.getUserIdInContext();
+		boolean isMember = teamMemberHelperService.isMemberTeam(currentUserId, teamId);
+		if (!teamHelperService.isLeaderOfTeam(currentUserId, teamId)) {
+			if (!isMember) {
+				throw new ForbiddenException("User does not have access");
+			}
 		}
-		Page<Task> tasks = listTasksByUser(userId, status, priority, startDate, endDate, pageable);
-		return result(tasks);
-	}
-
-	private Page<TaskResponse> result(Page<Task> tasks) {
-		List<Long> taskIds = tasks.map(Task::getId).toList();
-		Map<Long, Long> commentCounts = commentHelperService.countComment(taskIds);
-		Map<Long, Long> reportCounts = reportHelperService.countReport(taskIds);
-		return customResponse(tasks, commentCounts, reportCounts);
+		if (isMember) {
+			return listTaskByMember(currentUserId, teamId, status, priority, startDate, endDate, pageable);
+		}
+		return listTasksByLeader(currentUserId, teamId, status, priority, startDate, endDate, pageable);
 	}
 
 	@Override
-	public TaskResponse addUser(String taskId, String assigneer, String assignee) {
+	public TaskResponse updateStatus(String taskId, TaskRequest taskRequest) {
+		Long currentUserId = authService.getUserIdInContext();
 		return taskRepository.findById(idEncoder.decode(taskId)).map(task -> {
-			methodGeneral.validatePermission(idEncoder.decode(assigneer), task.getUser().getId());
-			return new TaskResponse(assignTaskToNewUser(task, assignee));
-		}).orElseThrow(() -> new ResourceNotFoundException("Request failed"));
-
-	}
-
-	@Override
-	public TaskResponse removeUser(String taskId, String assigneer, String assignee) {
-		return taskRepository.findById(idEncoder.decode(taskId)).map(task -> {
-			methodGeneral.validatePermission(idEncoder.decode(assigneer), task.getUser().getId());
-			return new TaskResponse(removeUserInTask(task, assignee));
-		}).orElseThrow(() -> new ResourceNotFoundException("Request failed"));
-	}
-
-	@Override
-	public String updateStatus(String taskId, String userId, TaskRequest taskRequest) {
-		taskRepository.findById(idEncoder.decode(taskId)).map(task -> {
-			if (task.getAssignTo() != null) {
-				methodGeneral.havePermission(idEncoder.decode(userId), task.getUser().getId(), task.getAssignTo().getId());
-			} else {
-				methodGeneral.validatePermission(idEncoder.decode(userId), task.getUser().getId());
+			if (!taskAssignmentHelperService.isUserInTask(currentUserId, taskId)) {
+				methodGeneral.validatePermission(currentUserId, task.getUser().getId());
 			}
 			task.setStatus(StatusEnum.valueOf(taskRequest.getStatus()));
 			taskRepository.save(task);
-			return "Success";
+			return new TaskResponse(taskRepository.save(task));
 		}).orElseThrow(() -> new ResourceNotFoundException("Request failed"));
-		return "False";
 	}
 
 	@Override
-	public String updatePriority(String taskId, String userId, TaskRequest taskRequest) {
-		taskRepository.findById(idEncoder.decode(taskId)).map(task -> {
-			methodGeneral.validatePermission(idEncoder.decode(userId), task.getUser().getId());
+	public TaskResponse updatePriority(String taskId, TaskRequest taskRequest) {
+		Long currentUserId = authService.getUserIdInContext();
+		return taskRepository.findById(idEncoder.decode(taskId)).map(task -> {
+			methodGeneral.validatePermission(currentUserId, task.getUser().getId());
 			task.setPriority(PriorityEnum.valueOf(taskRequest.getPriority()));
 			taskRepository.save(task);
-			return "Success";
+			return new TaskResponse(taskRepository.save(task));
 		}).orElseThrow(() -> new ResourceNotFoundException("Request failed"));
-		return "False";
 	}
 
 	@Override
-	public Page<TaskResponse> searchTasks(String userId, String keySearch, Boolean type, Pageable pageable) {
-		return taskRepository.searchTask(idEncoder.decode(userId), keySearch, type, pageable)
-				.map(t -> new TaskResponse(idEncoder.endcode(t.getId()), t.getTitle(), t.getStatus(), t.getDueAt(),
+	public Page<TaskResponse> searchTasks(String keyword, Pageable pageable) {
+		Long currentUserId = authService.getUserIdInContext();
+		return taskRepository.searchTask(currentUserId, keyword, pageable)
+				.map(t -> new TaskResponse(idEncoder.encode(t.getId()), t.getTitle(), t.getStatus(), t.getDueAt(),
 						t.getCommentCount(), t.getReportCount()));
 	}
 
-	private Task assignTaskToNewUser(Task task, String assignee) {
-		User user = userHelperService.userExist(assignee)
-				.orElseThrow(() -> new ResourceNotFoundException("User can not found"));
-		task.setAssignTo(user);
-		taskRepository.save(task);
-		pushTaskEvent(task, assignee, NotificationType.NEW_ASSIGN.toString());
-		return task;
+	@Override
+	public List<TaskStatusResponse> getTaskSummaryInTeam(String teamId) {
+		Long currentUserId = authService.getUserIdInContext();
+		return taskRepository.getTaskSummary(currentUserId, idEncoder.decode(teamId)).stream().map(taskSummary -> {
+			return new TaskStatusResponse(taskSummary.getStatus(), taskSummary.getTaskCount());
+		}).toList();
 	}
 
-	private Task removeUserInTask(Task task, String assignee) {
-		if (task.getAssignTo().getId() == idEncoder.decode(assignee)) {
-			task.setAssignTo(null);
-			task.setStatus(StatusEnum.CANCELLED);
-			taskRepository.save(task);
-			pushTaskEvent(task, assignee, NotificationType.REMOVE_ASSIGN.toString());
+	protected void isCreatorTask(Long userId, String taskId) {
+		Specification<Task> specification = Specification.where(
+				TaskSpecification.getTaskById(idEncoder.decode(taskId)).and(TaskSpecification.getTaskByUserId(userId)));
+		if (!taskRepository.exists(specification)) {
+			throw new ForbiddenException("User do not have permission");
 		}
-		return task;
 	}
 
-	private Page<Task> listTasksAssign(String userId, String status, String priority, String startDate, String endDate,
-			Pageable pageable) {
-		Specification<Task> specification = Specification.where(TaskSpecification
-				// by userId
-				.getTaskByUserId(idEncoder.decode(userId))
-				// by status
-				.and(TaskSpecification.getTaskByStatus(status)
-						// by priority
-						.and(TaskSpecification.getTaskByType(priority)
-								// assignee
-								.and(TaskSpecification.getTaskHaveAssign())
-								// between custom date
-								.and(TaskSpecification.getTaskByDate(ConvertData.toTimestamp(startDate),
-										ConvertData.toTimestamp(endDate))))));
-		return taskRepository.findAll(specification, pageable);
+	private Page<TaskResponse> listTaskByMember(Long userId, String teamId, String status, String priority,
+			String startDate, String endDate, Pageable pageable) {
+		return taskRepository.getTaskByMember(userId, idEncoder.decode(teamId), StatusEnum.valueOf(status),
+				PriorityEnum.valueOf(priority), ConvertData.toTimestamp(startDate), ConvertData.toTimestamp(endDate),
+				pageable).map(task -> {
+					return new TaskResponse(idEncoder.encode(task.getId()), task.getTitle(), task.getPriority(),
+							task.getDueAt(), task.getCommentCount(), task.getReportCount());
+				});
 	}
 
-	private Page<Task> listTasksByUser(String userId, String status, String priority, String startDate, String endDate,
-			Pageable pageable) {
-		Specification<Task> specification = Specification
-				// assigned
-				.where(TaskSpecification.getTaskAssign(idEncoder.decode(userId)))
-				// of this person not assign
-				.or(TaskSpecification.getTaskByUserId(idEncoder.decode(userId))
-						.and(TaskSpecification.getTaskNotAssign()))
-				.and(TaskSpecification.getTaskByStatus(status)).and(TaskSpecification.getTaskByType(priority))
-				.and(TaskSpecification.getTaskByDate(ConvertData.toTimestamp(startDate),
-						ConvertData.toTimestamp(endDate)));
-		return taskRepository.findAll(specification, pageable);
-	}
-
-	private Page<TaskResponse> customResponse(Page<Task> tasks, Map<Long, Long> commentCounts,
-			Map<Long, Long> reportCounts) {
-		return tasks.map(task -> {
-			Long commentCount = commentCounts.getOrDefault(task.getId(), 0L);
-			Long reportCount = reportCounts.getOrDefault(task.getId(), 0L);
-			TaskResponse taskResponse = new TaskResponse(task);
-			taskResponse.setCommentCount(commentCount);
-			taskResponse.setReportCount(reportCount);
-			return taskResponse;
-		});
-	}
-
-	private void pushTaskEvent(Task task, String assignee, String contentType) {
-		String senderName = Optional.ofNullable(task.getUser())
-				.map(user -> Optional.ofNullable(user.getFirstName()).orElse("") + " "
-						+ Optional.ofNullable(user.getLastName()).orElse(""))
-				.orElse("Unknown User");
-		applicationEventPublisher.publishEvent(new TaskEvent(task.getId(), task.getUser().getId(),
-				idEncoder.decode(assignee), senderName, task.getTitle(), contentType));
+	private Page<TaskResponse> listTasksByLeader(Long userId, String teamId, String status, String priority,
+			String startDate, String endDate, Pageable pageable) {
+		return taskRepository.getTaskByLeader(userId, idEncoder.decode(teamId), StatusEnum.valueOf(status),
+				PriorityEnum.valueOf(priority), ConvertData.toTimestamp(startDate), ConvertData.toTimestamp(endDate),
+				pageable).map(task -> {
+					return new TaskResponse(idEncoder.encode(task.getId()), task.getTitle(), task.getPriority(),
+							task.getDueAt(), task.getCommentCount(), task.getReportCount());
+				});
 	}
 }
