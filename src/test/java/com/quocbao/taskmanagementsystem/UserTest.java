1555900bbd19665f0479eaf82a4ca315c605c5a6
2uocbao <2uocbao.it@gmail.com>
2025-09-12 16:31:50 +0700

    Update user testing

--- src/test/java/com/quocbao/taskmanagementsystem/UserTest.java
+++ src/test/java/com/quocbao/taskmanagementsystem/UserTest.java
@@ -2,8 +2,12 @@ package com.quocbao.taskmanagementsystem;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,7 +16,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.quocbao.taskmanagementsystem.entity.User;
 import com.quocbao.taskmanagementsystem.exception.ResourceNotFoundException;
+import com.quocbao.taskmanagementsystem.payload.request.FcmRequest;
 import com.quocbao.taskmanagementsystem.repository.UserRepository;
+import com.quocbao.taskmanagementsystem.service.utils.AuthenticationService;
 import com.quocbao.taskmanagementsystem.serviceimpl.UserServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,35 +26,67 @@ public class UserTest {
 
 	@Mock
 	private UserRepository userRepository;
-	
+
+	@Mock
+	private AuthenticationService authenticationService;
+
 	@InjectMocks
 	private UserServiceImpl userService;
-	
+
+	@BeforeEach
+	void defaultData() {
+		Long userId = 1L;
+		when(authenticationService.getUserIdInContext()).thenReturn(userId);
+	}
+
 	@Test
 	void testGetUserByEmail_Success() {
 		String email = "abc@gmail.com";
 		User user = User.builder().email(email).build();
-		
+
 		when(userRepository.findByEmail(email)).thenReturn(user);
-		
+
 		User result = userService.getUserByEmail(email);
-		
-		assertNotNull(result);
+
 		assertEquals(email, result.getEmail());
-		verify(userRepository).findByEmail(email);
 	}
-	
+
 	@Test
 	void testGetUserByEmail_NotFound() {
-		 // Arrange
-        String email = "notfound@example.com";
-        when(userRepository.findByEmail(email)).thenReturn(null);
+		// Arrange
+		String email = "notfound@example.com";
+		when(userRepository.findByEmail(email)).thenReturn(null);
+		// Act & Assert
+		assertThrows(ResourceNotFoundException.class, () -> {
+			userService.getUserByEmail(email);
+		});
+		verify(userRepository).findByEmail(email);
+	}
 
-        // Act & Assert
-        assertThrows(ResourceNotFoundException.class, () -> {
-            userService.getUserByEmail(email);
-        });
+	@Test
+	void testUpdateFCMToken_Success() {
+		User user = User.builder().id(1L).build();
+		when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
 
-        verify(userRepository).findByEmail(email);
+		FcmRequest fcmRequest = new FcmRequest();
+		fcmRequest.setLanguage("vn");
+		fcmRequest.setFcmToken("network");
+
+		userService.addToken(fcmRequest);
+		System.out.println(user.getToken());
+		System.out.println(user.getLanguage());
+		verify(userRepository).findById(user.getId());
+		verify(userRepository).save(user);
+	}
+
+	@Test
+	void testRemoveFCMToken_Success() {
+		User user = User.builder().id(1L).token("fcmtoken").language("vn").build();
+		when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+		userService.removeToken();
+		// System.out.println(user.getToken());
+		// System.out.println(user.getLanguage());
+		verify(userRepository).findById(user.getId());
+		verify(userRepository).save(user);
 	}
 }
