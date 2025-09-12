376750bdf20182943d50344f7315cc372c9c33c5
2uocbao <2uocbao.it@gmail.com>
2025-09-12 22:23:35 +0700

    Update team test

--- src/test/java/com/quocbao/taskmanagementsystem/TeamTest.java
+++ src/test/java/com/quocbao/taskmanagementsystem/TeamTest.java
@@ -1,11 +1,18 @@
 package com.quocbao.taskmanagementsystem;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -23,7 +30,9 @@ import com.quocbao.taskmanagementsystem.exception.ResourceNotFoundException;
 import com.quocbao.taskmanagementsystem.payload.request.TeamRequest;
 import com.quocbao.taskmanagementsystem.payload.response.TeamResponse;
 import com.quocbao.taskmanagementsystem.repository.TeamRepository;
+import com.quocbao.taskmanagementsystem.repository.TeamRepository.TeamProjection;
 import com.quocbao.taskmanagementsystem.service.utils.AuthenticationService;
+import com.quocbao.taskmanagementsystem.service.utils.TaskHelperService;
 import com.quocbao.taskmanagementsystem.service.utils.UserHelperService;
 import com.quocbao.taskmanagementsystem.serviceimpl.TeamServiceImpl;
 
@@ -32,6 +41,9 @@ public class TeamTest {
 
 	@Mock
 	private TeamRepository teamRepository;
+
+	@Mock
+	private TaskHelperService taskHelperService;
 
 	@Mock
 	private UserHelperService userHelperService;
@@ -48,108 +60,210 @@ public class TeamTest {
 	@InjectMocks
 	private TeamServiceImpl teamService;
 
+	@BeforeEach
+	void defaultData() {
+		Long userId = 1L;
+		when(authService.getUserIdInContext()).thenReturn(userId);
+	}
+
 	@Test
 	void testCreateTeam_Success() {
-		Long userId = 1L;
-		User user = User.builder().id(userId).build();
-
-		when(authService.getUserIdInContext()).thenReturn(userId);
-
-		when(userHelperService.isUserExist(userId)).thenReturn(true);
-
+		when(userHelperService.isUserExist(anyLong())).thenReturn(true);
 		String teamName = "abc";
 		Long teamId = 1L;
-		Team team = Team.builder().id(teamId).leaderId(user).name(teamName).build();
-
+		Team team = Team.builder().id(teamId).leaderId(User.builder().id(1L).build()).name(teamName).build();
 		TeamRequest teamRequest = new TeamRequest();
 		teamRequest.setName(teamName);
-
 		when(teamRepository.save(any(Team.class))).thenReturn(team);
-
 		TeamResponse result = teamService.createTeam(teamRequest);
-
-		assertNotNull(result);
-
 		assertEquals(teamName, result.getName());
-
 		verify(teamRepository).save(any(Team.class));
 	}
 
 	@Test
 	void testCreateTeam_UserNotFound() {
-		Long userId = 1L;
-
-		when(authService.getUserIdInContext()).thenReturn(userId);
-
-		when(userHelperService.isUserExist(userId)).thenReturn(false);
-
-		assertThrows(ResourceNotFoundException.class, () -> {
+		when(userHelperService.isUserExist(1L)).thenReturn(false);
+		ResourceNotFoundException ex = assertThrows(ResourceNotFoundException.class, () -> {
 			teamService.createTeam(null);
 		});
+		assertEquals("User can not found", ex.getMessage());
 		verify(teamRepository, never()).save(any());
 	}
 
-	@SuppressWarnings("unchecked")
 	@Test
 	void testCreateTeam_TeamNameAlreadyExist() {
 		String teamName = "abc";
 		Long userId = 1L;
-		when(authService.getUserIdInContext()).thenReturn(userId);
 		when(userHelperService.isUserExist(userId)).thenReturn(true);
 		when(teamRepository.exists(any(Specification.class))).thenReturn(true);
-		assertThrows(DuplicateException.class, () -> {
+		DuplicateException ex = assertThrows(DuplicateException.class, () -> {
 			TeamRequest teamRequest = new TeamRequest();
 			teamRequest.setName(teamName);
 			teamService.createTeam(teamRequest);
 		});
+		assertEquals("abc already exist", ex.getMessage());
 		verify(teamRepository, never()).save(any());
 	}
 
 	@Test
 	void testUpdateTeam_Success() {
-		Long userId = 1L;
-		User user = User.builder().id(userId).build();
-		Team existingTeam = Team.builder().id(1L).leaderId(user).name("abc").build();
+		Team existingTeam = Team.builder().id(1L).leaderId(User.builder().id(1L).build()).name("abc").build();
 		TeamRequest teamRequest = new TeamRequest();
 		teamRequest.setName("abc2");
-		when(teamRepository.findById(any(Long.class))).thenReturn(Optional.of(existingTeam));
+		when(teamRepository.findById(anyLong())).thenReturn(Optional.of(existingTeam));
 		when(teamRepository.save(any(Team.class))).thenAnswer(inv -> inv.getArgument(0));
-
 		TeamResponse teamResponse = teamService.updateTeam(idEncoder.encode(existingTeam.getId()), teamRequest);
-
 		assertNotNull(teamResponse);
-
 		assertEquals(teamResponse.getName(), "abc2");
-
+		verify(teamRepository).findById(anyLong());
 		verify(teamRepository).save(existingTeam);
 	}
 
 	@Test
-	void testUpdateTeam_WhenUserIsNotLeader_ThrowsForbiddenException() {
+	void testUpdateTeam_NotFound() {
+		String teamId = "teamId";
+		Long teamIdLong = 1L;
+		when(idEncoder.decode(teamId)).thenReturn(teamIdLong);
+		when(teamRepository.findById(teamIdLong)).thenReturn(Optional.empty());
+		ResourceNotFoundException ex = assertThrows(ResourceNotFoundException.class, () -> {
+			teamService.deleteTeam(teamId);
+		});
+		assertEquals("This team not found", ex.getMessage());
+		verify(teamRepository).findById(teamIdLong);
+	}
+
+	@Test
+	void testUpdateTeam_ThrowsForbiddenException() {
 		Long teamId = 1L;
-		String encodedId = "abc123";
-		Long currentUserId = 99L;
-		Long leaderId = 1L;
-
-		TeamRequest request = new TeamRequest();
-		request.setName("New Name");
-
+		String teamIdString = "2L";
+		Long leaderId = 2L;
 		Team team = Team.builder()
 				.id(teamId)
 				.name("Old Name")
 				.leaderId(User.builder().id(leaderId).build())
 				.build();
-
-		when(authService.getUserIdInContext()).thenReturn(currentUserId);
-		when(idEncoder.decode(encodedId)).thenReturn(teamId);
+		when(idEncoder.decode(teamIdString)).thenReturn(1L);
 		when(teamRepository.findById(teamId)).thenReturn(Optional.of(team));
-		doThrow(new ForbiddenException("No permission"))
-				.when(methodGeneral).validatePermission(currentUserId, leaderId);
-
-		assertThrows(ForbiddenException.class, () -> {
-			teamService.updateTeam(encodedId, request);
+		doThrow(new ForbiddenException("User does not have permission."))
+				.when(methodGeneral).validatePermission(1L, leaderId);
+		TeamRequest teamRequest = new TeamRequest();
+		teamRequest.setName("abc2");
+		ForbiddenException ex = assertThrows(ForbiddenException.class, () -> {
+			teamService.updateTeam(teamIdString, teamRequest);
 		});
-
+		assertEquals("User does not have permission.", ex.getMessage());
+		verify(teamRepository).findById(teamId);
 		verify(teamRepository, never()).save(any());
+	}
+
+	@Test
+	void testUpdateTeam_TeamNameAlreadyExist() {
+		Long teamId = 1L;
+		String teamIdString = "2L";
+		Long leaderId = 2L;
+		Team team = Team.builder()
+				.id(teamId)
+				.name("Old Name 1")
+				.leaderId(User.builder().id(leaderId).build())
+				.build();
+		when(idEncoder.decode(teamIdString)).thenReturn(teamId);
+		when(teamRepository.findById(teamId)).thenReturn(Optional.of(team));
+		when(teamRepository.exists(any(Specification.class))).thenReturn(true);
+
+		DuplicateException ex = assertThrows(DuplicateException.class, () -> {
+			TeamRequest teamRequest = new TeamRequest();
+			teamRequest.setName("Old Name");
+			teamService.updateTeam(teamIdString, teamRequest);
+		});
+		assertEquals("Old Name already exist", ex.getMessage());
+		verify(teamRepository, never()).save(any());
+	}
+
+	@Test
+	void testDeleteTeam_Success() {
+		String teamId = "teamId";
+		Team team = Team.builder().id(1L).leaderId(User.builder().id(1L).build()).build();
+		when(idEncoder.decode(teamId)).thenReturn(team.getId());
+		when(taskHelperService.isTaskActive(anyLong(), anyString())).thenReturn(false);
+		when(teamRepository.findById(team.getId())).thenReturn(Optional.of(team));
+		teamService.deleteTeam(teamId);
+		verify(teamRepository).delete(team);
+	}
+
+	@Test
+	void testDeleteTeam_ForbiddenException() {
+		String teamId = "teamId";
+		Team team = Team.builder().id(1L).leaderId(User.builder().id(2L).build()).build();
+		when(idEncoder.decode(teamId)).thenReturn(team.getId());
+		when(taskHelperService.isTaskActive(anyLong(), anyString())).thenReturn(false);
+		when(teamRepository.findById(team.getId())).thenReturn(Optional.of(team));
+		doThrow(new ForbiddenException("User does not have permission."))
+				.when(methodGeneral).validatePermission(team.getLeaderId().getId(), 1L);
+		ForbiddenException ex = assertThrows(ForbiddenException.class, () -> {
+			teamService.deleteTeam(teamId);
+		});
+		assertEquals("User does not have permission.", ex.getMessage());
+		verify(teamRepository).findById(team.getId());
+		verify(teamRepository, never()).deleteById(team.getId());
+	}
+
+	@Test
+	void testDeleteTeam_AlreadyTaskActive() {
+		String teamId = "teamId";
+		Team team = Team.builder().id(1L).leaderId(User.builder().id(2L).build()).build();
+		when(idEncoder.decode(teamId)).thenReturn(team.getId());
+		when(taskHelperService.isTaskActive(anyLong(), anyString())).thenReturn(true);
+		when(teamRepository.findById(team.getId())).thenReturn(Optional.of(team));
+		DuplicateException ex = assertThrows(DuplicateException.class, () -> {
+			teamService.deleteTeam(teamId);
+		});
+		assertEquals("This team has not completed some tasks. Can not delete.", ex.getMessage());
+		verify(teamRepository).findById(team.getId());
+	}
+
+	@Test
+	void testRetriveTeams_NotNull() {
+		Long userId = 1L;
+		TeamProjection teamProjection = new TeamProjection() {
+
+			@Override
+			public Long getId() {
+				return 1L;
+			}
+
+			@Override
+			public String getName() {
+				return "Team";
+			}
+
+			@Override
+			public Timestamp getCreatedAt() {
+				return Timestamp.valueOf(LocalDateTime.now());
+			}
+
+			@Override
+			public Long getLeaderId() {
+				return 1L;
+			}
+
+			@Override
+			public String getFirstName() {
+				return "John";
+			}
+
+			@Override
+			public String getLastName() {
+				return "Doe";
+			}
+
+			@Override
+			public String getImage() {
+				return null;
+			}
+
+		};
+		when(teamRepository.getTeams(userId)).thenReturn(List.of(teamProjection));
+		List<TeamResponse> teamResponses = teamService.getTeams();
+		assertEquals(1, teamResponses.size());
 	}
 }
