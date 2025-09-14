27779aa2788826ce35b23c164ba8c372d6c33cc0
2uocbao <2uocbao.it@gmail.com>
2025-09-14 15:03:21 +0700

    Update team member specification

--- src/main/java/com/quocbao/taskmanagementsystem/specifications/TeamMemberSpecification.java
+++ src/main/java/com/quocbao/taskmanagementsystem/specifications/TeamMemberSpecification.java
@@ -2,6 +2,7 @@ package com.quocbao.taskmanagementsystem.specifications;
 
 import org.springframework.data.jpa.domain.Specification;
 
+import com.quocbao.taskmanagementsystem.common.RoleEnum;
 import com.quocbao.taskmanagementsystem.entity.TeamMember;
 import com.quocbao.taskmanagementsystem.entity.TeamMember_;
 import com.quocbao.taskmanagementsystem.entity.Team_;
@@ -16,13 +17,17 @@ public class TeamMemberSpecification {
 	public static Specification<TeamMember> getTeamMemberById(Long id) {
 		return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.get(TeamMember_.id), id);
 	}
-	
+
 	public static Specification<TeamMember> getTeamMemberByUserId(Long userId) {
 		return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.get(TeamMember_.user).get(User_.id), userId);
 	}
 
 	public static Specification<TeamMember> getTeamMemberByTeamId(Long teamId) {
 		return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.get(TeamMember_.team).get(Team_.id), teamId);
+	}
+
+	public static Specification<TeamMember> getTeamMemberByRole(RoleEnum role) {
+		return (root, _, criteriaBuilder) -> criteriaBuilder.equal(root.get(TeamMember_.role), role);
 	}
 
 }
