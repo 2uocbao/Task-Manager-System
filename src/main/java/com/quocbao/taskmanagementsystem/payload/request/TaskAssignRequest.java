6c58186f6f0b90e406a4d30ceea58362d09494c6
2uocbao <2uocbao.it@gmail.com>
2025-09-14 15:08:19 +0700

    Update task assignee request

--- src/main/java/com/quocbao/taskmanagementsystem/payload/request/TaskAssignRequest.java
+++ src/main/java/com/quocbao/taskmanagementsystem/payload/request/TaskAssignRequest.java
@@ -6,13 +6,6 @@ import lombok.Getter;
 
 @Getter
 public class TaskAssignRequest {
-
-	@JsonProperty("sender_name")
-	private String senderName;
-
-	@JsonProperty("task_title")
-	private String taskTitle;
-
 	@JsonProperty("to_userId")
 	private String toUserId;
 }
