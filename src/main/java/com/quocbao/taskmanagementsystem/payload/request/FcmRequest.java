12dd886df94fc4672d368a0002bf478943eaa1cf
2uocbao <2uocbao.it@gmail.com>
2025-09-14 15:07:54 +0700

    Update fcm request

--- src/main/java/com/quocbao/taskmanagementsystem/payload/request/FcmRequest.java
+++ src/main/java/com/quocbao/taskmanagementsystem/payload/request/FcmRequest.java
@@ -3,16 +3,18 @@ package com.quocbao.taskmanagementsystem.payload.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class FcmRequest {
 
 	@JsonProperty("userId")
 	private String userId;
-	
+
 	@JsonProperty("fcmToken")
 	private String fcmToken;
-	
+
 	@JsonProperty("language")
 	private String language;
 }
