bec54d8f193f43d324239b3dc0a218e6a6a95be7
2uocbao <2uocbao.it@gmail.com>
2025-09-15 22:05:23 +0700

    Update task service implement

--- src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/TaskServiceImpl.java
+++ src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/TaskServiceImpl.java
@@ -1,5 +1,6 @@
 package com.quocbao.taskmanagementsystem.serviceimpl;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.context.ApplicationEventPublisher;
@@ -7,20 +8,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.quocbao.taskmanagementsystem.common.ConvertData;
 import com.quocbao.taskmanagementsystem.common.IdEncoder;
-import com.quocbao.taskmanagementsystem.common.MethodGeneral;
-import com.quocbao.taskmanagementsystem.common.NotificationType;
 import com.quocbao.taskmanagementsystem.common.PriorityEnum;
+import com.quocbao.taskmanagementsystem.common.RoleEnum;
 import com.quocbao.taskmanagementsystem.common.StatusEnum;
 import com.quocbao.taskmanagementsystem.entity.Task;
 import com.quocbao.taskmanagementsystem.entity.Team;
 import com.quocbao.taskmanagementsystem.entity.User;
-import com.quocbao.taskmanagementsystem.events.DeleteEvent.NotifiDeletedEvent;
-import com.quocbao.taskmanagementsystem.events.DeleteEvent.ReportDeletedEvent;
-import com.quocbao.taskmanagementsystem.exception.DuplicateException;
+import com.quocbao.taskmanagementsystem.events.Assign.AssignEvent;
+import com.quocbao.taskmanagementsystem.events.Assign.HaveDeadlineEvent;
+import com.quocbao.taskmanagementsystem.exception.AccessDeniedException;
 import com.quocbao.taskmanagementsystem.exception.ForbiddenException;
 import com.quocbao.taskmanagementsystem.exception.ResourceNotFoundException;
 import com.quocbao.taskmanagementsystem.payload.request.TaskRequest;
@@ -35,7 +34,6 @@ import com.quocbao.taskmanagementsystem.service.utils.TeamMemberHelperService;
 import com.quocbao.taskmanagementsystem.specifications.TaskSpecification;
 
 @Service
-@Transactional
 public class TaskServiceImpl implements TaskService {
 
 	private final ApplicationEventPublisher applicationEventPublisher;
@@ -50,53 +48,48 @@ public class TaskServiceImpl implements TaskService {
 
 	private final AuthenticationService authService;
 
-	private final MethodGeneral methodGeneral;
-
 	private final IdEncoder idEncoder;
 
 	public TaskServiceImpl(ApplicationEventPublisher applicationEventPublisher, TaskRepository taskRepository,
 			TeamHelperService teamHelperService, TaskAssignmentHelperService taskAssignmentHelperService,
 			TeamMemberHelperService teamMemberHelperService, AuthenticationService authService,
-			MethodGeneral methodGeneral, IdEncoder idEncoder) {
+			IdEncoder idEncoder) {
 		this.applicationEventPublisher = applicationEventPublisher;
 		this.taskRepository = taskRepository;
 		this.teamHelperService = teamHelperService;
 		this.teamMemberHelperService = teamMemberHelperService;
 		this.taskAssignmentHelperService = taskAssignmentHelperService;
 		this.authService = authService;
-		this.methodGeneral = methodGeneral;
 		this.idEncoder = idEncoder;
 	}
 
 	@Override
 	public TaskResponse createTask(String teamId, TaskRequest taskRequest) {
-		try {
-			Long currentUserId = authService.getUserIdInContext();
-			if (!teamHelperService.isLeaderOfTeam(currentUserId, teamId)) {
-				throw new ForbiddenException("User can not create new task in this team");
-			}
-			Task task = new Task(taskRequest);
-			User user = User.builder().id(currentUserId).build();
-			Team team = Team.builder().id(idEncoder.decode(teamId)).build();
-			task.setTeam(team);
-			task.setUser(user);
-			task = taskRepository.save(task);
-			taskAssignmentHelperService.addTeamMember(currentUserId, task.getId());
-			return new TaskResponse(task);
-		} catch (Exception e) {
-			throw new ResourceNotFoundException();
+		Long currentUserId = authService.getUserIdInContext();
+		Long teamIdLong = idEncoder.decode(teamId);
+		if (!teamHelperService.isLeaderOfTeam(currentUserId, teamIdLong)) {
+			throw new ForbiddenException("User can not create new task in this team");
 		}
+		Task task = new Task(taskRequest);
+		User user = User.builder().id(currentUserId).build();
+		Team team = Team.builder().id(teamIdLong).build();
+		task.setTeam(team);
+		task.setUser(user);
+		task = taskRepository.save(task);
+		applicationEventPublisher.publishEvent(new AssignEvent(task.getId(), currentUserId));
+		return new TaskResponse(task);
 	}
 
 	@Override
 	public TaskResponse getTask(String taskId) {
 		Long currentUserId = authService.getUserIdInContext();
-		if (!taskAssignmentHelperService.isUserInTask(currentUserId, taskId)) {
-			isCreatorTask(currentUserId, taskId);
+		Long taskIdLong = idEncoder.decode(taskId);
+		if (!taskAssignmentHelperService.isUserInTask(currentUserId, taskIdLong)) {
+			throw new AccessDeniedException("User can not retrieve this task");
 		}
-		return taskRepository.findById(idEncoder.decode(taskId)).map(task -> {
+		return taskRepository.findById(taskIdLong).map(task -> {
 			TaskResponse taskResponse = new TaskResponse(task);
-			taskResponse.setId(idEncoder.encode(task.getId()));
+			taskResponse.setId(taskId);
 			taskResponse.setUserId(idEncoder.encode(task.getUser().getId()));
 			return taskResponse;
 		}).orElseThrow(() -> new ResourceNotFoundException("Task not found"));
@@ -105,55 +98,55 @@ public class TaskServiceImpl implements TaskService {
 	@Override
 	public TaskResponse updateTask(String taskId, TaskRequest taskRequest) {
 		Long currentUserId = authService.getUserIdInContext();
-		isCreatorTask(currentUserId, taskId);
-		return taskRepository.findById(idEncoder.decode(taskId)).map(task -> {
-			task = task.updateTask(taskRequest);
-			return new TaskResponse(taskRepository.save(task));
+		Long taskIdLong = idEncoder.decode(taskId);
+		if (!taskAssignmentHelperService.isRoleUserInTask(currentUserId, taskIdLong, RoleEnum.ADMIN)) {
+			throw new ForbiddenException("User do not have permission");
+		}
+		return taskRepository.findById(taskIdLong).map(task -> {
+			Task taskUpdate = task.updateTask(taskRequest);
+			return new TaskResponse(taskRepository.save(taskUpdate));
 		}).orElseThrow(() -> new ResourceNotFoundException("Task not found for update."));
 	}
 
 	@Override
-	@Transactional
 	public void deleteTask(String taskId) {
 		Long currentUserId = authService.getUserIdInContext();
 		Long taskIdLong = idEncoder.decode(taskId);
-		taskRepository.findById(taskIdLong).ifPresentOrElse(t -> {
-			isCreatorTask(currentUserId, taskId);
-			if (taskAssignmentHelperService.haveAssign(taskId))
-				throw new DuplicateException("Remove the member from this task before deleting it.");
-			applicationEventPublisher
-					.publishEvent(new NotifiDeletedEvent(taskIdLong, NotificationType.TASK.toString()));
-			applicationEventPublisher.publishEvent(new ReportDeletedEvent(taskIdLong));
-			taskRepository.delete(t);
-
-		}, () -> new ResourceNotFoundException("Not found task need delete."));
+		taskRepository.findById(taskIdLong).ifPresentOrElse(task -> {
+			if (taskAssignmentHelperService.isRoleUserInTask(currentUserId, taskIdLong, RoleEnum.ADMIN)) {
+				throw new ForbiddenException("User do not have permission");
+			}
+			taskRepository.delete(task);
+		}, () -> {
+			throw new ResourceNotFoundException("Not found task need delete.");
+		});
 	}
 
 	@Override
 	public Page<TaskResponse> getTasks(String teamId, String status, String priority, String startDate, String endDate,
 			Pageable pageable) {
 		Long currentUserId = authService.getUserIdInContext();
-		boolean isMember = teamMemberHelperService.isMemberTeam(currentUserId, teamId);
-		if (!teamHelperService.isLeaderOfTeam(currentUserId, teamId)) {
-			if (!isMember) {
-				throw new ForbiddenException("User does not have access");
-			}
+		Long teamIdLong = idEncoder.decode(teamId);
+		if (teamMemberHelperService.isMemberTeam(currentUserId, teamIdLong)) {
+			throw new AccessDeniedException("User can not access to list task of this team");
 		}
-		if (isMember) {
-			return listTaskByMember(currentUserId, teamId, status, priority, startDate, endDate, pageable);
-		}
-		return listTasksByLeader(currentUserId, teamId, status, priority, startDate, endDate, pageable);
+		return taskRepository.getTask(currentUserId, teamIdLong, StatusEnum.valueOf(status),
+				PriorityEnum.valueOf(priority), ConvertData.toTimestamp(startDate), ConvertData.toTimestamp(endDate),
+				pageable).map(task -> {
+					return new TaskResponse(idEncoder.encode(task.getId()), task.getTitle(), task.getPriority(),
+							task.getDueAt(), task.getCommentCount(), task.getReportCount());
+				});
 	}
 
 	@Override
 	public TaskResponse updateStatus(String taskId, TaskRequest taskRequest) {
 		Long currentUserId = authService.getUserIdInContext();
-		return taskRepository.findById(idEncoder.decode(taskId)).map(task -> {
-			if (!taskAssignmentHelperService.isUserInTask(currentUserId, taskId)) {
-				methodGeneral.validatePermission(currentUserId, task.getUser().getId());
-			}
+		Long taskIdLong = idEncoder.decode(taskId);
+		if (!taskAssignmentHelperService.isUserInTask(currentUserId, taskIdLong)) {
+			throw new AccessDeniedException("User can not update status of this task");
+		}
+		return taskRepository.findById(taskIdLong).map(task -> {
 			task.setStatus(StatusEnum.valueOf(taskRequest.getStatus()));
-			taskRepository.save(task);
 			return new TaskResponse(taskRepository.save(task));
 		}).orElseThrow(() -> new ResourceNotFoundException("Request failed"));
 	}
@@ -161,18 +154,21 @@ public class TaskServiceImpl implements TaskService {
 	@Override
 	public TaskResponse updatePriority(String taskId, TaskRequest taskRequest) {
 		Long currentUserId = authService.getUserIdInContext();
-		return taskRepository.findById(idEncoder.decode(taskId)).map(task -> {
-			methodGeneral.validatePermission(currentUserId, task.getUser().getId());
+		Long taskIdLong = idEncoder.decode(taskId);
+		if (!taskAssignmentHelperService.isRoleUserInTask(currentUserId, taskIdLong, RoleEnum.ADMIN)) {
+			throw new AccessDeniedException("User can not update priority of this task");
+		}
+		return taskRepository.findById(taskIdLong).map(task -> {
 			task.setPriority(PriorityEnum.valueOf(taskRequest.getPriority()));
-			taskRepository.save(task);
 			return new TaskResponse(taskRepository.save(task));
 		}).orElseThrow(() -> new ResourceNotFoundException("Request failed"));
 	}
 
 	@Override
-	public Page<TaskResponse> searchTasks(String keyword, Pageable pageable) {
+	public Page<TaskResponse> searchTasks(String teamId, String keyword, Pageable pageable) {
 		Long currentUserId = authService.getUserIdInContext();
-		return taskRepository.searchTask(currentUserId, keyword, pageable)
+		Long teamIdLong = idEncoder.decode(teamId);
+		return taskRepository.searchTask(teamIdLong, currentUserId, keyword, pageable)
 				.map(t -> new TaskResponse(idEncoder.encode(t.getId()), t.getTitle(), t.getStatus(), t.getDueAt(),
 						t.getCommentCount(), t.getReportCount()));
 	}
@@ -185,31 +181,12 @@ public class TaskServiceImpl implements TaskService {
 		}).toList();
 	}
 
-	protected void isCreatorTask(Long userId, String taskId) {
-		Specification<Task> specification = Specification.where(
-				TaskSpecification.getTaskById(idEncoder.decode(taskId)).and(TaskSpecification.getTaskByUserId(userId)));
-		if (!taskRepository.exists(specification)) {
-			throw new ForbiddenException("User do not have permission");
-		}
-	}
-
-	private Page<TaskResponse> listTaskByMember(Long userId, String teamId, String status, String priority,
-			String startDate, String endDate, Pageable pageable) {
-		return taskRepository.getTaskByMember(userId, idEncoder.decode(teamId), StatusEnum.valueOf(status),
-				PriorityEnum.valueOf(priority), ConvertData.toTimestamp(startDate), ConvertData.toTimestamp(endDate),
-				pageable).map(task -> {
-					return new TaskResponse(idEncoder.encode(task.getId()), task.getTitle(), task.getPriority(),
-							task.getDueAt(), task.getCommentCount(), task.getReportCount());
-				});
-	}
-
-	private Page<TaskResponse> listTasksByLeader(Long userId, String teamId, String status, String priority,
-			String startDate, String endDate, Pageable pageable) {
-		return taskRepository.getTaskByLeader(userId, idEncoder.decode(teamId), StatusEnum.valueOf(status),
-				PriorityEnum.valueOf(priority), ConvertData.toTimestamp(startDate), ConvertData.toTimestamp(endDate),
-				pageable).map(task -> {
-					return new TaskResponse(idEncoder.encode(task.getId()), task.getTitle(), task.getPriority(),
-							task.getDueAt(), task.getCommentCount(), task.getReportCount());
-				});
+	@Override
+	public void listSchudeledTasks(Timestamp startTime, Timestamp endTime) {
+		List<Task> tasks = taskRepository
+				.findAll(Specification.where(TaskSpecification.getTaskByDate(startTime, endTime)));
+		tasks.stream().forEach(task -> {
+			applicationEventPublisher.publishEvent(new HaveDeadlineEvent(task.getId()));
+		});
 	}
 }
