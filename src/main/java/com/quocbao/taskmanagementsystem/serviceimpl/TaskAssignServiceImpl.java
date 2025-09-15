318fa64454baea65f2da6ca87ce69302fc354ccb
2uocbao <2uocbao.it@gmail.com>
2025-09-15 23:58:08 +0700

    Update task assignment service implement

--- src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/TaskAssignServiceImpl.java
+++ src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/TaskAssignServiceImpl.java
@@ -14,7 +14,8 @@ import com.quocbao.taskmanagementsystem.common.RoleEnum;
 import com.quocbao.taskmanagementsystem.entity.Task;
 import com.quocbao.taskmanagementsystem.entity.TaskAssignment;
 import com.quocbao.taskmanagementsystem.entity.User;
-import com.quocbao.taskmanagementsystem.events.NotifiEvent.TaskEvent;
+import com.quocbao.taskmanagementsystem.events.Notification.NotificationAddEvent;
+import com.quocbao.taskmanagementsystem.exception.AccessDeniedException;
 import com.quocbao.taskmanagementsystem.exception.DuplicateException;
 import com.quocbao.taskmanagementsystem.exception.ForbiddenException;
 import com.quocbao.taskmanagementsystem.exception.ResourceNotFoundException;
@@ -25,7 +26,6 @@ import com.quocbao.taskmanagementsystem.service.TaskAssignService;
 import com.quocbao.taskmanagementsystem.service.utils.AuthenticationService;
 import com.quocbao.taskmanagementsystem.service.utils.TaskHelperService;
 import com.quocbao.taskmanagementsystem.service.utils.TeamMemberHelperService;
-import com.quocbao.taskmanagementsystem.service.utils.UserHelperService;
 import com.quocbao.taskmanagementsystem.specifications.TaskAssignSpecification;
 
 @Service
@@ -34,8 +34,6 @@ public class TaskAssignServiceImpl implements TaskAssignService {
 	private final ApplicationEventPublisher applicationEventPublisher;
 
 	private final TaskAssignmentRepository taskAssignmentRepository;
-
-	private final UserHelperService userHelperService;
 
 	private final TeamMemberHelperService teamMemberHelperService;
 
@@ -46,12 +44,11 @@ public class TaskAssignServiceImpl implements TaskAssignService {
 	private final IdEncoder idEncoder;
 
 	public TaskAssignServiceImpl(ApplicationEventPublisher applicationEventPublisher,
-			TaskAssignmentRepository taskAssignmentRepository, UserHelperService userHelperService,
+			TaskAssignmentRepository taskAssignmentRepository,
 			TeamMemberHelperService teamMemberHelperService, TaskHelperService taskHelperService,
 			AuthenticationService authService, IdEncoder idEncoder) {
 		this.applicationEventPublisher = applicationEventPublisher;
 		this.taskAssignmentRepository = taskAssignmentRepository;
-		this.userHelperService = userHelperService;
 		this.teamMemberHelperService = teamMemberHelperService;
 		this.taskHelperService = taskHelperService;
 		this.authService = authService;
@@ -61,38 +58,36 @@ public class TaskAssignServiceImpl implements TaskAssignService {
 	@Override
 	public TaskAssignResponse addAssign(String taskId, TaskAssignRequest taskAssignRequest) {
 		Long currentUserId = authService.getUserIdInContext();
+		Long taskIdLong = idEncoder.decode(taskId);
+		Long assignerId = idEncoder.decode(taskAssignRequest.getToUserId());
 		// User is creator task
-		if (!taskHelperService.isCreatorTask(currentUserId, taskId)) {
-			throw new ForbiddenException("User do not have permission");
+		if (!isAdmin(currentUserId, taskIdLong)) {
+			throw new AccessDeniedException("User do not have permission");
 		}
 
-		// Assigner is already exist in task
-		boolean delivered = taskAssignmentRepository.exists(Specification
-				.where(TaskAssignSpecification.getByUserAssign(idEncoder.decode(taskAssignRequest.getToUserId()))
-						.and(TaskAssignSpecification.getByTask(idEncoder.decode(taskId)))));
-		if (delivered) {
+		// Assignee is already exist in task
+
+		if (isAlreadyExist(currentUserId, taskIdLong)) {
 			throw new DuplicateException("User already exist in task");
 		}
 
 		// Get task
-		Task task = taskHelperService.getTask(taskId).orElseThrow(() -> {
+		Task task = taskHelperService.getTask(taskIdLong).orElseThrow(() -> {
 			throw new ResourceNotFoundException("Task can not find");
 		});
 
-		// Assigner have in team
-		Long assignerId = idEncoder.decode(taskAssignRequest.getToUserId());
-		if (!teamMemberHelperService.isMemberTeam(assignerId, idEncoder.encode(task.getTeam().getId()))) {
-			throw new ResourceNotFoundException("Assigner do not have in team");
+		// Assignee have in the team have this task
+		if (!teamMemberHelperService.isMemberTeam(assignerId, task.getTeam().getId())) {
+			throw new ForbiddenException("Assignee did not have in team have this task");
 		}
 
-		User user = userHelperService.getUser(assignerId).get();
+		User user = User.builder().id(assignerId).build();
 
 		TaskAssignment taskAssignment = taskAssignmentRepository.save(TaskAssignment.builder().task(task)
-				.user(User.builder().id(idEncoder.decode(taskAssignRequest.getToUserId())).build())
+				.user(User.builder().id(assignerId).build())
 				.role(RoleEnum.MEMBER).build());
 
-		sendNotificationAssign(currentUserId, user.getId(), taskId, taskAssignRequest.getTaskTitle(),
-				taskAssignRequest.getSenderName(), user.getToken() == null ? null : user.getToken(), user.getLanguage(),
+		sendNotificationAssign(currentUserId, user.getId(), taskIdLong,
 				NotificationType.NEW_ASSIGN.toString());
 
 		return new TaskAssignResponse(idEncoder.encode(taskAssignment.getId()), taskAssignRequest.getToUserId(),
@@ -102,38 +97,78 @@ public class TaskAssignServiceImpl implements TaskAssignService {
 	}
 
 	@Override
-	public Boolean removeAssign(String taskId, String assignId, TaskAssignRequest taskAssignRequest) {
+	public void removeAssign(String taskId, String assignId, TaskAssignRequest taskAssignRequest) {
 		Long currentUserId = authService.getUserIdInContext();
-		if (!taskHelperService.isCreatorTask(currentUserId, taskId)) {
-			return false;
+		Long assignIdLong = idEncoder.decode(assignId);
+		Long taskIdLong = idEncoder.decode(taskId);
+		if (!isAdmin(currentUserId, taskIdLong)) {
+			throw new AccessDeniedException("User do not have permission");
 		}
-		taskAssignmentRepository.findById(idEncoder.decode(assignId)).ifPresent(taskAssign -> {
-			taskAssignmentRepository.deleteById(idEncoder.decode(assignId));
-			Optional.ofNullable(userHelperService.getUser(taskAssign.getUser().getId()).get()).ifPresent(user -> {
-				sendNotificationAssign(currentUserId, user.getId(), taskId, taskAssignRequest.getTaskTitle(),
-						taskAssignRequest.getSenderName(), user.getToken() == null ? null : user.getToken(),
-						user.getLanguage(), NotificationType.REMOVE_ASSIGN.toString());
-			});
+		taskAssignmentRepository.findById(assignIdLong).ifPresent(taskAssign -> {
+			if (taskAssign.getUser().getId() == currentUserId) {
+				throw new ForbiddenException("Can not do this");
+			}
+			taskAssignmentRepository.delete(taskAssign);
+			sendNotificationAssign(currentUserId, taskAssign.getUser().getId(), taskIdLong,
+					NotificationType.REMOVE_ASSIGN.toString());
 		});
-		return true;
 	}
 
 	@Override
 	public List<TaskAssignResponse> getTaskAssigns(String taskId) {
-		return taskAssignmentRepository.getTaskAssignments(idEncoder.decode(taskId)).stream().map(taskAssignment -> {
-			String username = Optional.ofNullable(taskAssignment.getFirstName()).orElse("") + " "
-					+ Optional.ofNullable(taskAssignment.getLastName()).orElse("");
-			String image = Optional.ofNullable(taskAssignment.getImage()).orElse("");
-			return new TaskAssignResponse(idEncoder.encode(taskAssignment.getId()),
-					idEncoder.encode(taskAssignment.getAssignerId()), username, taskAssignment.getMention(), image,
-					taskAssignment.getRole(),
-					ConvertData.timeStampToString(taskAssignment.getJoinedAt()));
-		}).toList();
+		Long currentUserId = authService.getUserIdInContext();
+		Long taskIdL = idEncoder.decode(taskId);
+		if (!isAlreadyExist(taskIdL, currentUserId)) {
+			throw new AccessDeniedException("User do not have permission");
+		}
+		return taskAssignmentRepository.getTaskAssignments(taskIdL).stream()
+				.map(taskAssignment -> {
+					String username = Optional.ofNullable(taskAssignment.getFirstName()).orElse("") + " "
+							+ Optional.ofNullable(taskAssignment.getLastName()).orElse("");
+					String image = Optional.ofNullable(taskAssignment.getImage()).orElse("");
+					return new TaskAssignResponse(idEncoder.encode(taskAssignment.getId()),
+							idEncoder.encode(taskAssignment.getAssignerId()), username, taskAssignment.getMention(),
+							image,
+							taskAssignment.getRole(),
+							ConvertData.timeStampToString(taskAssignment.getJoinedAt()));
+				}).toList();
 	}
 
-	protected void sendNotificationAssign(Long senderId, Long receiverId, String taskId, String taskTitle,
-			String senderName, String token, String language, String contentType) {
-		applicationEventPublisher.publishEvent(new TaskEvent(idEncoder.decode(taskId), senderId, receiverId, senderName,
-				taskTitle, contentType, token, language));
+	@Override
+	public void assigneeHaveDeadline(Long taskId) {
+		List<TaskAssignment> taskAssignments = taskAssignmentRepository
+				.findAll(Specification.where(TaskAssignSpecification.getByTask(taskId)));
+		if (taskAssignments.size() == 1) {
+			sendNotificationAssign(taskId, taskAssignments.getFirst().getUser().getId(), taskId,
+					NotificationType.DUEAT.toString());
+		} else {
+			taskAssignments.stream().forEach(assignee -> {
+				if (!assignee.getRole().equals(RoleEnum.ADMIN))
+					sendNotificationAssign(taskId, assignee.getUser().getId(), taskId,
+							NotificationType.DUEAT.toString());
+			});
+		}
+
+	}
+
+	protected Specification customSpecification(Long userId, Long taskId) {
+		return Specification.where(TaskAssignSpecification.getByTask(taskId))
+				.and(TaskAssignSpecification.getByUserAssign(userId));
+	}
+
+	protected Boolean isAlreadyExist(Long taskId, Long userId) {
+		return taskAssignmentRepository.exists(customSpecification(userId, taskId));
+	}
+
+	protected Boolean isAdmin(Long taskId, Long userId) {
+		return taskAssignmentRepository.exists(Specification
+				.where(customSpecification(userId, taskId)
+						.and(TaskAssignSpecification.getByRole(RoleEnum.ADMIN))));
+	}
+
+	protected void sendNotificationAssign(Long senderId, Long receiverId, Long taskId, String contentType) {
+		applicationEventPublisher
+				.publishEvent(new NotificationAddEvent(senderId, receiverId, taskId, NotificationType.TASK.toString(),
+						contentType));
 	}
 }
