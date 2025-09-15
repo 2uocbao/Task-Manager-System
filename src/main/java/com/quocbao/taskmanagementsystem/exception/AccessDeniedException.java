18ea90e3113e4cae5c7f54ee8c4eea679b11d1fe
2uocbao <2uocbao.it@gmail.com>
2025-09-16 00:05:44 +0700

    Create access denied exception

---
+++ src/main/java/com/quocbao/taskmanagementsystem/exception/AccessDeniedException.java
@@ -0,0 +1,16 @@
+package com.quocbao.taskmanagementsystem.exception;
+
+import java.io.Serial;
+
+public class AccessDeniedException extends RuntimeException {
+
+    /**
+     * 
+     */
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
