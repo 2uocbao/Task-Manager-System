ec674fd248c9eed2f8c5fb50f83cf1f9cdc89582
2uocbao <2uocbao.it@gmail.com>
2025-09-16 17:02:15 +0700

    Update report service implement

--- src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/ReportServiceImpl.java
+++ src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/ReportServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.event.EventListener;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.domain.Specification;
@@ -17,17 +16,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.quocbao.taskmanagementsystem.common.ConvertData;
 import com.quocbao.taskmanagementsystem.common.IdEncoder;
-import com.quocbao.taskmanagementsystem.common.MethodGeneral;
-import com.quocbao.taskmanagementsystem.common.NotificationType;
 import com.quocbao.taskmanagementsystem.common.StorageProperties;
 import com.quocbao.taskmanagementsystem.common.TypeEnum;
 import com.quocbao.taskmanagementsystem.entity.Report;
 import com.quocbao.taskmanagementsystem.entity.Task;
-import com.quocbao.taskmanagementsystem.entity.TaskAssignment;
 import com.quocbao.taskmanagementsystem.entity.User;
-import com.quocbao.taskmanagementsystem.events.DeleteEvent.ReportDeletedEvent;
-import com.quocbao.taskmanagementsystem.events.NotifiEvent.ReportEvent;
-import com.quocbao.taskmanagementsystem.exception.ForbiddenException;
+import com.quocbao.taskmanagementsystem.events.Assign.HaveReportEvent;
+import com.quocbao.taskmanagementsystem.exception.AccessDeniedException;
 import com.quocbao.taskmanagementsystem.exception.ResourceNotFoundException;
 import com.quocbao.taskmanagementsystem.payload.request.ReportRequest;
 import com.quocbao.taskmanagementsystem.payload.response.ReportResponse;
@@ -36,7 +31,6 @@ import com.quocbao.taskmanagementsystem.service.ReportService;
 import com.quocbao.taskmanagementsystem.service.utils.AuthenticationService;
 import com.quocbao.taskmanagementsystem.service.utils.TaskAssignmentHelperService;
 import com.quocbao.taskmanagementsystem.service.utils.TaskHelperService;
-import com.quocbao.taskmanagementsystem.service.utils.UserHelperService;
 import com.quocbao.taskmanagementsystem.specifications.ReportSpecification;
 
 @Service
@@ -50,8 +44,6 @@ public class ReportServiceImpl implements ReportService {
 
 	private final TaskAssignmentHelperService taskAssignmentHelperService;
 
-	private final UserHelperService userHelperService;
-
 	private final AuthenticationService authService;
 
 	private final IdEncoder idEncoder;
@@ -60,21 +52,17 @@ public class ReportServiceImpl implements ReportService {
 
 	private final ApplicationEventPublisher applicationEventPublisher;
 
-	private final MethodGeneral methodGeneral;
-
 	public ReportServiceImpl(ReportRepository reportRepository, TaskHelperService taskHelperService,
-			TaskAssignmentHelperService taskAssignmentHelperService, UserHelperService userHelperService,
+			TaskAssignmentHelperService taskAssignmentHelperService,
 			AuthenticationService authService, IdEncoder idEncoder, StorageProperties storageProperties,
-			ApplicationEventPublisher applicationEventPublisher, MethodGeneral methodGeneral) {
+			ApplicationEventPublisher applicationEventPublisher) {
 		this.reportRepository = reportRepository;
 		this.taskHelperService = taskHelperService;
 		this.taskAssignmentHelperService = taskAssignmentHelperService;
-		this.userHelperService = userHelperService;
 		this.authService = authService;
 		this.idEncoder = idEncoder;
 		this.storageProperties = storageProperties;
 		this.applicationEventPublisher = applicationEventPublisher;
-		this.methodGeneral = methodGeneral;
 	}
 
 	TypeEnum typeEnum = TypeEnum.FILE;
@@ -82,11 +70,11 @@ public class ReportServiceImpl implements ReportService {
 	@Override
 	public ReportResponse createReportForFile(MultipartFile file, String taskId) throws IOException {
 		Long currentUserId = authService.getUserIdInContext();
-		Boolean hasAccess = isMemberInTask(taskId, currentUserId) || isLeaderInTask(taskId, currentUserId);
-		if (!hasAccess) {
-			throw new ForbiddenException("User do not have access");
+		Long taskIdLong = idEncoder.decode(taskId);
+		if (!isMemberInTask(taskIdLong, currentUserId)) {
+			throw new AccessDeniedException("User do not have access");
 		}
-		if (!taskHelperService.isTaskExist(taskId)) {
+		if (!taskHelperService.isTaskExist(taskIdLong)) {
 			throw new ResourceNotFoundException("Task not found");
 		}
 
@@ -94,51 +82,52 @@ public class ReportServiceImpl implements ReportService {
 
 		file.transferTo(filePath.toFile());
 
-		LOGGER.info(file.getOriginalFilename());
 		if (Boolean.TRUE.equals(ConvertData.isImage(file.getOriginalFilename()))) {
 			typeEnum = TypeEnum.PHOTO;
 		}
 
 		User user = User.builder().id(currentUserId).build();
-		Task task = Task.builder().id(idEncoder.decode(taskId)).build();
+		Task task = Task.builder().id(taskIdLong).build();
 		Report report = Report.builder().user(user).task(task).fileName(file.getOriginalFilename())
 				.filePath(filePath.toString()).type(typeEnum).build();
 
 		Report reportResult = reportRepository.save(report);
 
-		sendNotificationToMember(taskId, currentUserId);
+		sendNotificationToMember(taskIdLong, currentUserId);
 		return new ReportResponse(reportResult);
 	}
 
 	@Override
 	public ReportResponse createReportForLink(ReportRequest reportRequest) {
 		Long currentUserId = authService.getUserIdInContext();
-		Boolean hasAccess = isMemberInTask(reportRequest.getTaskId(), currentUserId)
-				|| isLeaderInTask(reportRequest.getTaskId(), currentUserId);
-		if (!hasAccess) {
-			throw new ForbiddenException("User do not have access");
+		Long taskId = idEncoder.decode(reportRequest.getTaskId());
+		if (!isMemberInTask(taskId, currentUserId)) {
+			throw new AccessDeniedException("User do not have access");
 		}
-		if (!taskHelperService.isTaskExist(reportRequest.getTaskId())) {
+		if (!taskHelperService.isTaskExist(taskId)) {
 			throw new ResourceNotFoundException("Task not found");
 		}
 		Report report = Report.builder().user(User.builder().id(currentUserId).build())
-				.task(Task.builder().id(idEncoder.decode(reportRequest.getTaskId())).build())
+				.task(Task.builder().id(taskId).build())
 				.externalUrl(reportRequest.getExternalUrl()).type(TypeEnum.URL).build();
 		Report reportResult = reportRepository.save(report);
-		sendNotificationToMember(reportRequest.getTaskId(), currentUserId);
+		sendNotificationToMember(taskId, currentUserId);
 		return new ReportResponse(reportResult);
 	}
 
 	@Override
-	public void deleteReport(String reportId, String taskId) {
+	public void deleteReport(String reportId) {
 		Long currentUserId = authService.getUserIdInContext();
-		reportRepository.findById(idEncoder.decode(reportId)).ifPresentOrElse(report -> {
-			if (!isLeaderInTask(taskId, currentUserId)) {
-				methodGeneral.validatePermission(currentUserId, report.getUser().getId());
+		Long reportIdLong = idEncoder.decode(reportId);
+
+		reportRepository.findById(reportIdLong).ifPresentOrElse(report -> {
+			if (!taskHelperService.isCreatorTask(currentUserId, report.getTask().getId())) {
+				throw new AccessDeniedException("User do not have permission");
 			}
 			if (report.getType().equals(TypeEnum.PHOTO) || report.getType().equals(TypeEnum.FILE)) {
+				String taskIdString = idEncoder.encode(report.getTask().getId());
 				try {
-					Path filePath = Path.of(storageProperties.getLocation(), taskId + report.getFileName());
+					Path filePath = Path.of(storageProperties.getLocation(), taskIdString + report.getFileName());
 					Files.deleteIfExists(filePath);
 					LOGGER.info("Delete file: " + report.getFileName());
 				} catch (IOException e) {
@@ -146,12 +135,19 @@ public class ReportServiceImpl implements ReportService {
 				}
 			}
 			reportRepository.delete(report);
-		}, () -> new ResourceNotFoundException("Report not found"));
+		}, () -> {
+			throw new ResourceNotFoundException("Report not found");
+		});
 
 	}
 
 	@Override
 	public List<ReportResponse> getReports(String taskId) {
+		Long currentUserId = authService.getUserIdInContext();
+		Long taskIdL = idEncoder.decode(taskId);
+		if (!isMemberInTask(taskIdL, currentUserId)) {
+			throw new AccessDeniedException("User do not have access");
+		}
 		Specification<Report> specification = Specification
 				.where(ReportSpecification.getReportByTaskId(idEncoder.decode(taskId)));
 		List<Report> reportPage = reportRepository.findAll(specification,
@@ -162,63 +158,21 @@ public class ReportServiceImpl implements ReportService {
 	@Override
 	public Path getFile(String taskId, String reportId) {
 		Long currentUserId = authService.getUserIdInContext();
-		Boolean hasAccess = isMemberInTask(taskId, currentUserId) || isLeaderInTask(taskId, currentUserId);
-		if (!hasAccess) {
-			throw new ForbiddenException("User do not have access");
+		Long reportIdLong = idEncoder.decode(reportId);
+		Long taskIdLong = idEncoder.decode(taskId);
+		if (!isMemberInTask(taskIdLong, currentUserId)) {
+			throw new AccessDeniedException("User do not have access");
 		}
-		return reportRepository.findById(idEncoder.decode(reportId)).map(report -> {
+		return reportRepository.findById(reportIdLong).map(report -> {
 			return Path.of(storageProperties.getLocation()).resolve(taskId + report.getFileName());
 		}).orElseThrow(() -> new ResourceNotFoundException("File not found"));
 	}
 
-	protected Boolean isMemberInTask(String taskId, Long userId) {
+	protected Boolean isMemberInTask(Long taskId, Long userId) {
 		return taskAssignmentHelperService.isUserInTask(userId, taskId);
 	}
 
-	protected Boolean isLeaderInTask(String taskId, Long userId) {
-		return taskHelperService.isCreatorTask(userId, taskId);
-	}
-
-	protected void sendNotificationToMember(String taskId, Long userId) {
-		User userReported = userHelperService.getUser(userId).get();
-		Task task = taskHelperService.getTask(taskId).get();
-		String userName = userReported.getFirstName() + " " + userReported.getLastName();
-		String taskTitle = task.getTitle();
-		// send notification to leader
-		if (userId != task.getUser().getId()) {
-			applicationEventPublisher.publishEvent(new ReportEvent(task.getId(), userReported.getId(),
-					task.getUser().getId(), userName, taskTitle, NotificationType.REPORT.toString(),
-					task.getUser().getToken() == null ? null : task.getUser().getToken(),
-					task.getUser().getLanguage()));
-		}
-		// Fetch member in task
-		List<TaskAssignment> taskAssignments = taskAssignmentHelperService.getAssignmentsByTaskId(taskId);
-		taskAssignments.stream().forEach(taskAssign -> {
-			// send notification
-			if (taskAssign.getUser().getId() != userReported.getId()) {
-				applicationEventPublisher.publishEvent(new ReportEvent(task.getId(), userReported.getId(),
-						taskAssign.getUser().getId(), userName, taskTitle, NotificationType.REPORT.toString(),
-						taskAssign.getUser().getToken() == null ? null : taskAssign.getUser().getToken(),
-						taskAssign.getUser().getLanguage()));
-			}
-		});
-	}
-
-	@EventListener
-	public void deleteReport(ReportDeletedEvent event) {
-		String currentTaskId = idEncoder.encode(event.getTaskId());
-		List<Report> reports = reportRepository.findAll(ReportSpecification.getReportByTaskId(event.getTaskId()));
-		reports.stream().forEach(report -> {
-			if (report.getType().equals(TypeEnum.PHOTO) || report.getType().equals(TypeEnum.FILE)) {
-				try {
-					Path filePath = Path.of(storageProperties.getLocation(), currentTaskId + report.getFileName());
-					Files.deleteIfExists(filePath);
-					LOGGER.info("Delete file: " + report.getFileName());
-				} catch (IOException e) {
-					LOGGER.error("Error deleting file: " + e.getMessage());
-				}
-			}
-			reportRepository.delete(report);
-		});
+	protected void sendNotificationToMember(Long taskId, Long userId) {
+		applicationEventPublisher.publishEvent(new HaveReportEvent(userId, taskId));
 	}
 }
