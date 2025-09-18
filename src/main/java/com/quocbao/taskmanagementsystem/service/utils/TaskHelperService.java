634110570ffd56659ebc81ee745a7c234de97e8c
2uocbao <2uocbao.it@gmail.com>
2025-09-18 17:47:12 +0700

    Update task helper service

--- src/main/java/com/quocbao/taskmanagementsystem/service/utils/TaskHelperService.java
+++ src/main/java/com/quocbao/taskmanagementsystem/service/utils/TaskHelperService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-import com.quocbao.taskmanagementsystem.common.IdEncoder;
 import com.quocbao.taskmanagementsystem.entity.Task;
 import com.quocbao.taskmanagementsystem.repository.TaskRepository;
 import com.quocbao.taskmanagementsystem.specifications.TaskSpecification;
@@ -15,36 +14,34 @@ import com.quocbao.taskmanagementsystem.specifications.TaskSpecification;
 public class TaskHelperService {
 
 	private final TaskRepository taskRepository;
-	private final IdEncoder idEncoder;
 
-	public TaskHelperService(TaskRepository taskRepository, IdEncoder idEncoder) {
+	public TaskHelperService(TaskRepository taskRepository) {
 		this.taskRepository = taskRepository;
-		this.idEncoder = idEncoder;
 	}
 
-	public Optional<Task> getTask(String taskId) {
-		return taskRepository.findById(idEncoder.decode(taskId));
+	public Optional<Task> getTask(Long taskId) {
+		return taskRepository.findById(taskId);
 	}
 
-	public Boolean isTaskExist(String taskId) {
-		return taskRepository.exists(Specification.where(TaskSpecification.getTaskById(idEncoder.decode(taskId))));
+	public Boolean isTaskExist(Long taskId) {
+		return taskRepository.exists(Specification.where(TaskSpecification.getTaskById(taskId)));
 	}
 
-	public Boolean isCreatorTask(Long userId, String taskId) {
-		return taskRepository.exists(Specification.where(TaskSpecification.getTaskById(idEncoder.decode(taskId))
+	public Boolean isCreatorTask(Long userId, Long taskId) {
+		return taskRepository.exists(Specification.where(TaskSpecification.getTaskById(taskId)
 				.and(TaskSpecification.getTaskByUserId(userId))));
 	}
 
 	public List<Task> getTaskBetweenDate(Timestamp startAt, Timestamp endAt) {
 		return taskRepository.findAll(TaskSpecification.getTaskByDate(startAt, endAt));
 	}
-	
-	public List<Task> getTaskByTeamId(String teamId)  {
-		return taskRepository.findAll(TaskSpecification.getTaskByTeamId(idEncoder.decode(teamId)));
+
+	public List<Task> getTaskByTeamId(Long teamId) {
+		return taskRepository.findAll(TaskSpecification.getTaskByTeamId(teamId));
 	}
 
-	public Boolean isTaskActive(String teamId, String status) {
-		return taskRepository.exists(Specification.where(TaskSpecification.getTaskByTeamId(idEncoder.decode(teamId))
+	public Boolean isTaskActive(Long teamId, String status) {
+		return taskRepository.exists(Specification.where(TaskSpecification.getTaskByTeamId(teamId)
 				.and(TaskSpecification.getTaskNotHaveStatus(status))));
 	}
 }
