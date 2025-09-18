69aff294ff443265ac9b1b2019d4ad4ccb74c19a
2uocbao <2uocbao.it@gmail.com>
2025-09-18 17:46:53 +0700

    Update contact helper service

--- src/main/java/com/quocbao/taskmanagementsystem/service/utils/ContactHelperService.java
+++ src/main/java/com/quocbao/taskmanagementsystem/service/utils/ContactHelperService.java
@@ -3,7 +3,6 @@ package com.quocbao.taskmanagementsystem.service.utils;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import com.quocbao.taskmanagementsystem.common.IdEncoder;
 import com.quocbao.taskmanagementsystem.common.StatusEnum;
 import com.quocbao.taskmanagementsystem.entity.Contacts;
 import com.quocbao.taskmanagementsystem.repository.ContactRepository;
@@ -14,18 +13,15 @@ public class ContactHelperService {
 
 	private final ContactRepository contactRepository;
 
-	private final IdEncoder idEncoder;
-
-	public ContactHelperService(ContactRepository contactRepository, IdEncoder idEncoder) {
+	public ContactHelperService(ContactRepository contactRepository) {
 		this.contactRepository = contactRepository;
-		this.idEncoder = idEncoder;
 	}
 
-	public Boolean isConnected(Long userId, String userId1) {
+	public Boolean isConnected(Long userId, Long userId1) {
 		Specification<Contacts> specification = Specification.where(
 				(ContactSpecification.findContactByUserId(userId)
-						.and(ContactSpecification.findContactReceive(idEncoder.decode(userId1))))
-						.or(ContactSpecification.findContactByUserId(idEncoder.decode(userId1))
+						.and(ContactSpecification.findContactReceive(userId1)))
+						.or(ContactSpecification.findContactByUserId(userId1)
 								.and(ContactSpecification.findContactReceive(userId)))
 						.and(ContactSpecification.findContactByStatus(StatusEnum.ACCEPTED.toString())));
 		return contactRepository.exists(specification);
