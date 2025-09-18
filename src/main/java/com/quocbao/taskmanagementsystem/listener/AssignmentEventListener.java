b3f9dcee4540164426dcc51c109cb83eb1ffe301
2uocbao <2uocbao.it@gmail.com>
2025-09-18 14:54:47 +0700

    Create assignment event listener

---
+++ src/main/java/com/quocbao/taskmanagementsystem/listener/AssignmentEventListener.java
@@ -0,0 +1,24 @@
+package com.quocbao.taskmanagementsystem.listener;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import com.quocbao.taskmanagementsystem.events.Assign.HaveDeadlineEvent;
+import com.quocbao.taskmanagementsystem.service.TaskAssignService;
+
+@Component
+public class AssignmentEventListener {
+
+    private final TaskAssignService taskAssignService;
+
+    public AssignmentEventListener(TaskAssignService taskAssignService) {
+        this.taskAssignService = taskAssignService;
+    }
+
+    @Async("event_notifi")
+    @EventListener
+    public void getAssignerHaveDealine(HaveDeadlineEvent HaveDeadlineEvent) {
+        taskAssignService.assigneeHaveDeadline(HaveDeadlineEvent.getTaskId());
+    }
+}
