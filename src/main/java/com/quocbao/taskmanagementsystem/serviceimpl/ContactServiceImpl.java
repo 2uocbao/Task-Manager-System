e8d27a0339fcf67534209c6e1e8c3a06f868b4a2
2uocbao <2uocbao.it@gmail.com>
2025-09-18 14:44:50 +0700

    Update contact service implement

--- src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/ContactServiceImpl.java
+++ src/main/java/com/quocbao/taskmanagementsystem/serviceimpl/ContactServiceImpl.java
@@ -3,6 +3,7 @@ package com.quocbao.taskmanagementsystem.serviceimpl;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import com.quocbao.taskmanagementsystem.common.IdEncoder;
 import com.quocbao.taskmanagementsystem.common.MethodGeneral;
@@ -10,23 +11,21 @@ import com.quocbao.taskmanagementsystem.common.NotificationType;
 import com.quocbao.taskmanagementsystem.common.StatusEnum;
 import com.quocbao.taskmanagementsystem.entity.Contacts;
 import com.quocbao.taskmanagementsystem.entity.User;
-import com.quocbao.taskmanagementsystem.events.NotifiEvent.ContactEvent;
+import com.quocbao.taskmanagementsystem.events.Notification.NotificationAddEvent;
+import com.quocbao.taskmanagementsystem.exception.AccessDeniedException;
 import com.quocbao.taskmanagementsystem.exception.ResourceNotFoundException;
 import com.quocbao.taskmanagementsystem.payload.request.ContactRequest;
-import com.quocbao.taskmanagementsystem.payload.request.UpdateContactRequest;
 import com.quocbao.taskmanagementsystem.payload.response.ContactResponse;
 import com.quocbao.taskmanagementsystem.repository.ContactRepository;
 import com.quocbao.taskmanagementsystem.service.ContactService;
 import com.quocbao.taskmanagementsystem.service.utils.AuthenticationService;
-import com.quocbao.taskmanagementsystem.service.utils.NotifiHelperService;
 import com.quocbao.taskmanagementsystem.service.utils.UserHelperService;
+import com.quocbao.taskmanagementsystem.specifications.ContactSpecification;
 
 @Service
 public class ContactServiceImpl implements ContactService {
 
 	private final ContactRepository contactRepository;
-
-	private final NotifiHelperService notifiHelperService;
 
 	private final UserHelperService userHelperService;
 
@@ -38,11 +37,10 @@ public class ContactServiceImpl implements ContactService {
 
 	private final ApplicationEventPublisher applicationEventPublisher;
 
-	public ContactServiceImpl(ContactRepository contactRepository, NotifiHelperService notifiHelperService,
+	public ContactServiceImpl(ContactRepository contactRepository,
 			UserHelperService userHelperService, AuthenticationService authService, IdEncoder idEncoder,
 			MethodGeneral methodGeneral, ApplicationEventPublisher applicationEventPublisher) {
 		this.contactRepository = contactRepository;
-		this.notifiHelperService = notifiHelperService;
 		this.userHelperService = userHelperService;
 		this.authService = authService;
 		this.idEncoder = idEncoder;
@@ -55,44 +53,41 @@ public class ContactServiceImpl implements ContactService {
 		Long currentUserId = authService.getUserIdInContext();
 
 		Long toUserId = idEncoder.decode(contactRequest.getToUser());
-		Boolean isConnected = contactRepository.isConnected(currentUserId,
-				idEncoder.decode(contactRequest.getToUser()));
+		Boolean isConnected = contactRepository.exists(Specification.where((ContactSpecification
+				.findContactByUserId(currentUserId).and(ContactSpecification.findContactReceive(toUserId)))
+				.or(ContactSpecification
+						.findContactByUserId(toUserId).and(ContactSpecification.findContactReceive(currentUserId)))));
 		if (isConnected || currentUserId == toUserId) {
-			return;
+			throw new AccessDeniedException("Request do not access");
 		}
-		if (userHelperService.isUserExist(currentUserId)) {
+		if (userHelperService.isUserExist(toUserId)) {
 			User fromUser = User.builder().id(currentUserId).build();
-			User toUser = userHelperService.getUser(toUserId).get();
-			Contacts contacts = Contacts.builder().user(fromUser).friendId(toUser).statusEnum(StatusEnum.REQUESTED)
+			User toUser = User.builder().id(toUserId).build();
+			Contacts contactBuilder = Contacts.builder().user(fromUser).friendId(toUser)
+					.statusEnum(StatusEnum.REQUESTED)
 					.build();
-			Contacts contact = contactRepository.save(contacts);
-
-			applicationEventPublisher
-					.publishEvent(new ContactEvent(currentUserId, idEncoder.decode(contactRequest.getToUser()),
-							contact.getId(), contactRequest.getSenderName(), NotificationType.CONTACT.toString(),
-							toUser.getLanguage(), toUser.getToken() == null ? null : toUser.getToken()));
-
+			Contacts contact = contactRepository.save(contactBuilder);
+			publishNotificationEvent(currentUserId, toUserId, contact.getId(), NotificationType.REQUEST.toString());
 		} else {
 			throw new ResourceNotFoundException("Can not add contact");
 		}
 	}
 
 	@Override
-	public void updateContact(String contactId, UpdateContactRequest updateContactRequest) {
+	public void updateContact(String contactId) {
 		Long currentUserId = authService.getUserIdInContext();
-		contactRepository.findById(idEncoder.decode(contactId)).ifPresentOrElse(contact -> {
-			methodGeneral.validatePermission(currentUserId, contact.getFriendId().getId());
-			contact.setStatusEnum(StatusEnum.valueOf(updateContactRequest.getStatus()));
-			notifiHelperService.deleteNotification(contact.getId(), NotificationType.CONTACT.toString());
+		Long contactIdLong = idEncoder.decode(contactId);
+		contactRepository.findById(contactIdLong).ifPresentOrElse(contact -> {
+			if (currentUserId != contact.getFriendId().getId() || currentUserId == contact.getUser().getId()) {
+				throw new AccessDeniedException("User do not have access");
+			}
+			contact.setStatusEnum(StatusEnum.ACCEPTED);
 			contactRepository.save(contact);
-			userHelperService.getUser(idEncoder.decode(updateContactRequest.getToUser())).ifPresent(user -> {
-				applicationEventPublisher.publishEvent(new ContactEvent(currentUserId,
-						idEncoder.decode(updateContactRequest.getToUser()), contact.getId(),
-						updateContactRequest.getSenderName(), NotificationType.CONTACTACEPT.toString(),
-						user.getLanguage(), user.getToken() == null ? null : user.getToken()));
-			});
-
-		}, () -> new ResourceNotFoundException("Contact not found"));
+			publishNotificationEvent(currentUserId, contact.getUser().getId(), contactIdLong,
+					NotificationType.CONTACTACEPT.toString());
+		}, () -> {
+			throw new ResourceNotFoundException("Contact not found");
+		});
 	}
 
 	@Override
@@ -100,11 +95,10 @@ public class ContactServiceImpl implements ContactService {
 		Long currentUserId = authService.getUserIdInContext();
 		contactRepository.findById(idEncoder.decode(id)).ifPresentOrElse(contact -> {
 			methodGeneral.havePermission(currentUserId, contact.getUser().getId(), contact.getFriendId().getId());
-			notifiHelperService.deleteNotification(contact.getId(), NotificationType.CONTACT.toString());
 			contactRepository.delete(contact);
-
-		}, () -> new ResourceNotFoundException("Contact not found"));
-
+		}, () -> {
+			throw new ResourceNotFoundException("Contact not found");
+		});
 	}
 
 	@Override
@@ -157,5 +151,10 @@ public class ContactServiceImpl implements ContactService {
 					return new ContactResponse(teamId, idEncoder.encode(t.getUserId()), t.getFirstName(),
 							t.getLastName(), t.getEmail(), t.getImage(), null);
 				});
+	}
+
+	protected void publishNotificationEvent(Long senderId, Long receiverId, Long contentId, String contentType) {
+		applicationEventPublisher.publishEvent(new NotificationAddEvent(senderId, receiverId, contentId,
+				NotificationType.CONTACT.toString(), contentType));
 	}
 }
