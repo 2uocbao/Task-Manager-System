75c9bcccfadd020d5b122772189f03bef8f23f11
2uocbao <2uocbao.it@gmail.com>
2025-09-18 14:43:07 +0700

    Update contact request

--- src/main/java/com/quocbao/taskmanagementsystem/payload/request/ContactRequest.java
+++ src/main/java/com/quocbao/taskmanagementsystem/payload/request/ContactRequest.java
@@ -3,8 +3,10 @@ package com.quocbao.taskmanagementsystem.payload.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class ContactRequest {
 
 	@JsonProperty("to_user")
