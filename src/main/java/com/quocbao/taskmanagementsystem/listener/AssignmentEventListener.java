ea24c9376c0d98bd1d7281cc0ec13cc88c9562d0
2uocbao <2uocbao.it@gmail.com>
2025-10-01 20:13:41 +0700

    Update assignment event listener

--- src/main/java/com/quocbao/taskmanagementsystem/listener/AssignmentEventListener.java
+++ src/main/java/com/quocbao/taskmanagementsystem/listener/AssignmentEventListener.java
@@ -1,5 +1,7 @@
 package com.quocbao.taskmanagementsystem.listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,8 @@ import com.quocbao.taskmanagementsystem.service.TaskAssignService;
 @Component
 public class AssignmentEventListener {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AssignmentEventListener.class);
+
     private final TaskAssignService taskAssignService;
 
     public AssignmentEventListener(TaskAssignService taskAssignService) {
@@ -21,11 +25,13 @@ public class AssignmentEventListener {
     @EventListener
     public void getAssignerHaveDealine(HaveDeadlineEvent HaveDeadlineEvent) {
         taskAssignService.assigneeHaveDeadline(HaveDeadlineEvent.getTaskId());
+        LOGGER.info("Running in: " + Thread.currentThread().getName());
     }
 
     @Async("event_notifi")
     @EventListener
     public void addCreatorTask(AssignEvent assignEvent) {
         taskAssignService.createAdminTask(assignEvent.getUserId(), assignEvent.getTaskId());
+        LOGGER.info("Running in: " + Thread.currentThread().getName());
     }
 }
