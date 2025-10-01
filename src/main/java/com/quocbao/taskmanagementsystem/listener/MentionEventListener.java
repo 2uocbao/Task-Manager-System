899b0b4bd427bbab9fc342d798fd6be5b5be950d
2uocbao <2uocbao.it@gmail.com>
2025-10-01 20:14:07 +0700

    Update mention event listener

--- src/main/java/com/quocbao/taskmanagementsystem/listener/MentionEventListener.java
+++ src/main/java/com/quocbao/taskmanagementsystem/listener/MentionEventListener.java
@@ -1,35 +1,32 @@
 package com.quocbao.taskmanagementsystem.listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import com.quocbao.taskmanagementsystem.events.Mention.MentionAddEvent;
-import com.quocbao.taskmanagementsystem.events.Mention.MentionUpdateEvent;
-import com.quocbao.taskmanagementsystem.service.MentionService;
+import com.quocbao.taskmanagementsystem.service.utils.MentionHelperService;
 
 @Component
 public class MentionEventListener {
 
-    private final MentionService mentionService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MentionEventListener.class);
+
+    private final MentionHelperService mentionHelperService;
 
     public MentionEventListener(
-            MentionService mentionService) {
-        this.mentionService = mentionService;
+            MentionHelperService mentionHelperService) {
+        this.mentionHelperService = mentionHelperService;
     }
 
     @Async("event_notifi")
     @EventListener
     public void addMention(MentionAddEvent addMentionEvent) {
-        mentionService.createMention(addMentionEvent.getUserId(),
-                addMentionEvent.getCommentId(),
-                addMentionEvent.getMentionId());
-    }
-
-    @Async("event_notifi")
-    @EventListener
-    public void updateMentionByCommentId(MentionUpdateEvent mentionUpdateEvent) {
-        mentionService.updateMention(mentionUpdateEvent.getSender(), mentionUpdateEvent.getCommentId(),
-                mentionUpdateEvent.getUserId());
+        LOGGER.info("Running in: " + Thread.currentThread().getName());
+        mentionHelperService.getMentionAndPushNotiEvent(addMentionEvent.getUserId(),
+                addMentionEvent.getTaskId(),
+                addMentionEvent.getText());
     }
 }
