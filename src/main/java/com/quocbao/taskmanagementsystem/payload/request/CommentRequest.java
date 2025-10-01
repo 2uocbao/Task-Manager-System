b1a8210009d411a7a79db7abc25ffb0989e246ca
2uocbao <2uocbao.it@gmail.com>
2025-10-01 20:15:13 +0700

    Update comment request

--- src/main/java/com/quocbao/taskmanagementsystem/payload/request/CommentRequest.java
+++ src/main/java/com/quocbao/taskmanagementsystem/payload/request/CommentRequest.java
@@ -1,7 +1,5 @@
 package com.quocbao.taskmanagementsystem.payload.request;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
@@ -10,9 +8,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CommentRequest {
-
-	@JsonProperty("mention")
-	private List<String> mention;
 
 	@JsonProperty("text")
 	private String text;
